794e0022d43d5d4dd876ce77b4af5ff97246742d
Christopher Jung <bktheg@web.de>
2006-11-16 13:32:50 +0100

    Tick-Basisklassen eingebaut
    committer: Christopher Jung <bktheg@web.de>

---
+++ src/net/driftingsouls/ds2/server/tick/AbstractTickExecuter.java
@@ -0,0 +1,188 @@
+/*
+ *	Drifting Souls 2
+ *	Copyright (c) 2006 Christopher Jung
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation; either
+ *	version 2.1 of the License, or (at your option) any later version.
+ *
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package net.driftingsouls.ds2.server.tick;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.impl.LogFactoryImpl;
+
+import net.driftingsouls.ds2.server.ContextCommon;
+import net.driftingsouls.ds2.server.framework.BasicContext;
+import net.driftingsouls.ds2.server.framework.CmdLineRequest;
+import net.driftingsouls.ds2.server.framework.Common;
+import net.driftingsouls.ds2.server.framework.Configuration;
+import net.driftingsouls.ds2.server.framework.DriftingSouls;
+import net.driftingsouls.ds2.server.framework.SimpleResponse;
+
+/**
+ * Klasse zur Ausfuehrung von mehreren Ticks
+ * @author Christopher Jung
+ *
+ */
+public abstract class AbstractTickExecuter extends TickController {
+	private String loxpath = Configuration.getSetting("LOXPATH");
+	private String name = "";
+	
+	/**
+	 * Bootet DS
+	 * Als Kommandozeilenparameter <code>--config</code> wird der Konfigurationspfad erwartet
+	 * @param args Die Kommandozeilenargumente
+	 * @throws Exception
+	 */
+	public static final void boot(String[] args) throws Exception {
+		System.getProperties().setProperty("org.apache.commons.logging.Log","org.apache.commons.logging.impl.SimpleLog");
+		
+		Log LOG = new LogFactoryImpl().getInstance("DS2");
+		LOG.info("Booting DS...");
+		
+		CmdLineRequest request = new CmdLineRequest(args);
+		
+		try {
+			new DriftingSouls(LOG, request.getParameterString("config"), false);
+		}
+		catch( Exception e ) {
+			LOG.fatal(e, e);
+			throw new Exception(e);
+		}
+		
+		SimpleResponse response = new SimpleResponse();
+		new BasicContext(request, response);
+	}
+	
+	/**
+	 * Setzt den Namen des Ticks. Dieser wird in LOXPATH/tix.log festgehalten
+	 * @param name Name des Ticks
+	 */
+	protected void setName( String name ) {
+		this.name = name;
+	}
+	
+	/**
+	 * Setzt den Basispfad fuer alle Logdateien. Default
+	 * ist LOXPATH.
+	 * 
+	 * @param path Basispfad fuer Logs
+	 */
+	protected void setLogPath( String path ) {
+		this.loxpath = path;
+		if( loxpath.charAt(loxpath.length()-1) != '/' ) {
+			loxpath += '/';
+		}
+	}
+	
+	/**
+	 * Fuehrt ein Tickscript aus
+	 * @param tickname Eine Instanz des Tickscripts
+	 * @param useSTDOUT Soll STDOUT oder eine Logdatei mit dem Namen des Ticks verwendet werden?
+	 */
+	protected void execTick( Class<? extends TickController> tickname, boolean useSTDOUT ) throws Exception {
+		TickController tick = tickname.newInstance();
+			
+		if( !useSTDOUT ) {
+			tick.addLogTarget(loxpath+tickname.getSimpleName().toLowerCase()+".log", false);
+		}
+		else {
+			tick.addLogTarget(STDOUT, false);
+		}
+				
+		tick.execute();
+		tick.dispose();
+	}
+	
+	/**
+	 * Kopiert alle Logs im Tick-Log-Verzeichnis in ein Unterverzeichnis mit der Tick-Nr
+	 * @param $ticknr Nummer des Ticks
+	 */
+	protected void copyLogs( int ticknr ) {
+		File[] files = new File(loxpath).listFiles();
+		for( int i=0; i < files.length; i++ ) {
+			if( files[i].getName().substring(files[i].getName().length()-4).equals(".log") ) {
+				String filename = files[i].getName();
+				if( filename.lastIndexOf('/') > -1 ) {
+					filename = filename.substring(filename.lastIndexOf('/')+1);
+				}
+				Common.copyFile(files[i].getAbsolutePath(), loxpath+ticknr+"/"+filename);
+			}
+		}
+	}
+	
+	/**
+	 * Schreibt den aktuellen Status nach ticktime.log (befindet sich unter LOXPATH
+	 * und nicht im Basisverzeichnis der Tick-Logs!)
+	 * @param status Der zu schreibende Status
+	 */
+	protected void publishStatus( String status ) {
+		try {
+			int handle = addLogTarget(Configuration.getSetting("LOXPATH")+"ticktime.log", false);
+			log("Bitte warten - "+status);
+			removeLogTarget(handle);
+		}
+		catch( IOException e ) {
+			System.err.println("Tickstatus konnte nicht publiziert werden: "+e);
+		}
+	}
+	
+	/**
+	 * Vor- und Nachbereitung der Tickausfuehrung
+	 * @throws Exception 
+	 * @throws ClassNotFoundException 
+	 */
+	@Override
+	final protected void tick() {
+		if( getContext().getRequest().getParameterString("only").length() > 0 ) {
+			
+			try {
+				execTick(Class.forName(getContext().getRequest().getParameterString("only")).asSubclass(TickController.class), true);
+			} catch (Exception e) {
+				System.err.println("Ausfuehrung des Ticks "+getContext().getRequest().getParameterString("only")+" fehlgeschlagen: "+e);
+			}
+		}
+		
+		int ticknr = getContext().get(ContextCommon.class).getTick()+1;
+		
+		if( new File(loxpath+"/"+ticknr).mkdir() ) {
+			return;
+		}
+		
+		executeTicks();
+		
+		copyLogs(ticknr);
+		
+		try {
+			int handle = addLogTarget(Configuration.getSetting("LOXPATH")+"tix.log", true);
+			if( name.length() != 0 ) {
+				slog(name+": ");
+			}
+			int handle2 = addLogTarget(Configuration.getSetting("LOXPATH")+"ticktime.log", false);
+			log(Common.date("d.m.Y H:i:s"));
+			removeLogTarget(handle2);
+			removeLogTarget(handle);
+		}
+		catch( Exception e ) {
+			System.err.println("Fehler bei der Ticknachbereitung: "+e);
+		}
+	}
+	
+	/**
+	 * Fuehrt alle Einzelticks aus
+	 */
+	protected abstract void executeTicks();
+}
---
+++ src/net/driftingsouls/ds2/server/tick/TickController.java
@@ -0,0 +1,250 @@
+/*
+ *	Drifting Souls 2
+ *	Copyright (c) 2006 Christopher Jung
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation; either
+ *	version 2.1 of the License, or (at your option) any later version.
+ *
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package net.driftingsouls.ds2.server.tick;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.driftingsouls.ds2.server.framework.Common;
+import net.driftingsouls.ds2.server.framework.Context;
+import net.driftingsouls.ds2.server.framework.ContextMap;
+import net.driftingsouls.ds2.server.framework.db.Database;
+
+/**
+ * Basisklasse fuer Ticks
+ * @author Christopher Jung
+ *
+ */
+public abstract class TickController {
+	/**
+	 * Log-Ziel: Standardausgabe
+	 */
+	public static final String STDOUT = "java://STDOUT";
+			
+	private long exectime;
+	private Map<Integer,Writer> logTargets;
+	private int handleCounter;
+	private Context context;
+	
+	/**
+	 * Erstellt eine neue Instanz
+	 */
+	public TickController() {
+		logTargets = new HashMap<Integer,Writer>();
+		handleCounter = 0;
+		this.context = ContextMap.getContext();
+	
+		exectime = System.currentTimeMillis();
+	}
+	
+	/**
+	 * Beendet den Tick und gibt alle Resourcen wieder frei
+	 */
+	public void dispose() {
+		for( int handle : logTargets.keySet() ) {
+			try {
+				removeLogTarget(handle);
+			}
+			catch( IOException e ) {
+				// EMPTY
+			}
+		}
+	}
+	
+	/**
+	 * Hier koennen Vorbereitungen getroffen werden.
+	 * Sollten Fehler auftreten wird der Tick hiernach abgebrochen.
+	 */
+	protected abstract void prepare();
+	
+	/**
+	 * Der eigendliche Tick...
+	 */
+	protected abstract void tick();
+	
+	/**
+	 * Blockiert den angegebenen Spieler bzw alle Spieler. Unter
+	 * blockieren ist hierbei die Fehlermeldung "Im Moment werden
+	 * einige Tick-Berechnungen durchgefuehrt" zu verstehen.
+	 * 
+	 * @param userid Der zu sperrende User oder 0 (default) fuer alle User
+	 */
+	protected void block(int userid) {
+		Database db = getDatabase();
+		
+		if( userid != 0 ) {
+			db.update("UPDATE sessions SET tick=1 WHERE id='",userid,"'");
+		}
+		else {
+			db.update("UPDATE sessions SET tick=1");
+		}
+	}
+	
+	/**
+	 * Entsperrt den angegebenen Spieler bzw alle Spieler.
+	 * @see #block(userid)
+	 * 
+	 * @param userid Der zu entsperrende User oder 0 (default) fuer alle User
+	 */
+	protected void unblock( int userid ) {
+		Database db = getDatabase();
+		
+		if( userid != 0 ) {
+			db.update("UPDATE sessions SET tick=0 WHERE id='",userid,"'");
+		}
+		else {
+			db.update("UPDATE sessions SET tick=0");
+		}
+	}
+	
+	/**
+	 * Startet die Tickausfuehrung
+	 */
+	public void execute() {
+		log("-----------------"+Common.date("d.m.Y H:i:s")+"-------------------");
+		
+		prepare();
+		if( getErrorList().length == 0 ) {
+			tick();
+		}
+		
+		if( getErrorList().length > 0 ) {
+			log("");
+			log("Fehlerliste:");
+
+			for( net.driftingsouls.ds2.server.framework.pipeline.Error error : getErrorList() ) {
+				slog("* ");
+				log(error.getDescription());
+			}
+		}
+		
+		if( context.getResponse().getContent().length() > 0 ) {
+			log("\n-------------Weitere Ausgaben---------------\n");
+			log(context.getResponse().getContent().toString());
+		}
+		context.getResponse().setContent("");
+		
+		log("");
+		log("QCount: "+getDatabase().getQCount());
+		
+		log("Execution-Time: "+(System.currentTimeMillis()-exectime)+"s");
+	}
+	
+	/**
+	 * Loggt einen String
+	 * @param $string Der zu loggende String
+	 */
+	protected void slog(String string) {
+		for( int i : logTargets.keySet() ) {
+			try {
+				logTargets.get(i).write(string);
+			}
+			catch( IOException e ) {
+				System.err.println("Fehler beim Schreiben - schliesse Handler: "+e);
+				try{
+					removeLogTarget(i);
+				}
+				catch(IOException f) {
+					// EMPTY
+				}
+			}
+		}
+	}
+	
+	/**
+	 * Loggt eine Zeile. Fuer den Zeilenumbruch wird automatisch gesorgt
+	 * @param $string Die zu loggende Zeile
+	 */
+	protected void log(String string) {
+		slog(string+"\n");
+	}
+	
+	/**
+	 * Fuegt ein neues Ziel fuer geloggte Daten hinzu
+	 * @param file Das Ziel, zu dem geloggt werden soll. Das Ziel muss schreibbar sein
+	 * @param append Sollen die Daten angehangen werden?
+	 * 
+	 * @return Handle des Log-Ziels oder -1 (gescheitert)
+	 */
+	public int addLogTarget( String file, boolean append ) throws IOException {
+		Writer w = null;
+		if( file.equals(STDOUT) ) {
+			w = new OutputStreamWriter(System.out);
+		}
+		else {
+			File f = new File(file);
+			if( !f.exists() ) {
+				f.createNewFile();
+			}
+			w = new FileWriter(f, append);
+		}
+	
+		if( w == null ) {
+			return -1;
+		}
+		
+		logTargets.put(handleCounter, w);
+		
+		handleCounter++;
+		
+		return (handleCounter-1);
+	}
+	
+	/**
+	 * Entfernt ein Ziel fuer geloggte Daten
+	 * @param handle Das Handle des Log-Ziels
+	 * 
+	 * @return true bei erfolgreichem entfernen
+	 */
+	public boolean removeLogTarget( int handle ) throws IOException {		
+		logTargets.get(handle).close();
+		logTargets.remove(handle);
+
+		return true;
+	}
+	
+	/**
+	 * Gibt den aktuellen Context zurueck
+	 * @return der Kontext
+	 */
+	public Context getContext() {
+		return context;
+	}
+	
+	/**
+	 * Gibt eine Datenbankinstanz des Kontexts zurueck
+	 * @return eine Datenbankinstanz
+	 */
+	public Database getDatabase() {
+		return context.getDatabase();
+	}
+	
+	/**
+	 * Gibt die Fehlerliste des Kontexts zurueck
+	 * @return die Fehlerliste
+	 */
+	public net.driftingsouls.ds2.server.framework.pipeline.Error[] getErrorList() {
+		return context.getErrorList();
+	}
+}
