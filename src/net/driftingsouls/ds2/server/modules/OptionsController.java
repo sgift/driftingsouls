f91ee8b760f41c7c83c6ddd826e1089b03db4fe6
Christopher Jung <bktheg@web.de>
2006-11-25 18:41:07 +0100

    Weitere Aktionen implementiert
    committer: Christopher Jung <bktheg@web.de>

--- src/net/driftingsouls/ds2/server/modules/OptionsController.java
+++ src/net/driftingsouls/ds2/server/modules/OptionsController.java
@@ -134,8 +134,84 @@ public class OptionsController extends DSGenerator {
 	 * 
 	 */
 	public void changeXtraAction() {
-		// TODO
-		Common.stub();
+		User user = getUser();
+		TemplateEngine t = getTemplateEngine();
+		
+		parameterNumber("shipgroupmulti");
+		parameterNumber("inttutorial");
+		parameterNumber("scriptdebug");
+		parameterNumber("mapwidth");
+		parameterNumber("mapheight");
+		parameterNumber("defrelation");
+		
+		int shipgroupmulti = getInteger("shipgroupmulti");
+		int inttutorial = getInteger("inttutorial");
+		int scriptdebug = getInteger("scriptdebug");
+		int mapwidth = getInteger("mapwidth");
+		int mapheight = getInteger("mapheight");
+		int defrelation = getInteger("defrelation");
+		
+		User.Relation rel = User.Relation.NEUTRAL;
+		switch( defrelation ) {
+		case 1: 
+			rel = User.Relation.ENEMY;
+			break;
+		case 2:
+			rel = User.Relation.FRIEND;
+			break;
+		}
+		
+		String changemsg = "";
+
+		if( shipgroupmulti != Integer.parseInt(user.getUserValue("TBLORDER/schiff/wrapfactor")) ) {
+			changemsg += "Neuer Schiffsgruppenmultiplikator gespeichert...<br />\n";
+			
+			user.setUserValue( "TBLORDER/schiff/wrapfactor", Integer.toString(shipgroupmulti) );
+		}
+		
+		if( (scriptdebug != 0) && (user.getAccessLevel() >= 20) ) {
+			parameterNumber("scriptdebugstatus");
+			
+			boolean scriptdebugstatus = getInteger("scriptdebugstatus") != 0 ? true : false;
+			
+			if( scriptdebugstatus != user.hasFlag( User.FLAG_SCRIPT_DEBUGGING ) ) {	
+				user.setFlag( User.FLAG_SCRIPT_DEBUGGING, scriptdebugstatus  );		
+				
+				changemsg += "Scriptdebugging "+(scriptdebugstatus ? "" : "de")+"aktiviert<br />\n";
+			}
+		}
+		
+		if( inttutorial != Integer.parseInt(user.getUserValue("TBLORDER/uebersicht/inttutorial")) ) {
+			if( inttutorial != 0 ) {
+				changemsg += "Tutorial aktiviert...<br />\n";
+			}
+			else {
+				changemsg += "Tutorial deaktiviert...<br />\n";
+			}
+			user.setUserValue("TBLORDER/uebersicht/inttutorial", Integer.toString(inttutorial) );
+		}
+		
+		if( mapwidth != Integer.parseInt(user.getUserValue("TBLORDER/map/width")) ) {
+			changemsg += "Kartenbreite ge&auml;ndert...<br />\n";
+
+			user.setUserValue("TBLORDER/map/width", Integer.toString(mapwidth) );
+		}
+		
+		if( mapheight != Integer.parseInt(user.getUserValue("TBLORDER/map/height")) ) {
+			changemsg += "Kartenh&ouml;he ge&auml;ndert...<br />\n";
+
+			user.setUserValue("TBLORDER/map/height", Integer.toString(mapheight));
+		}
+		
+		if( rel != user.getRelation(0) ) {
+			changemsg += "Diplomatiehaltung ge&auml;ndert...<br />\n";
+
+			user.setRelation(0,rel);
+		}
+		
+		t.set_var( "options.message", changemsg );
+		
+		redirect("xtra");
 	}
 	
 	/**
@@ -170,8 +246,37 @@ public class OptionsController extends DSGenerator {
 	 *
 	 */
 	public void gfxPakAction() {
-		// TODO
-		Common.stub();
+		User user = getUser();
+		TemplateEngine t = getTemplateEngine();
+		
+		parameterString("gfxpak");
+		
+		String gfxpak = getString("gfxpak");
+
+		if( gfxpak.length() == 0 ) {
+			user.setImagePath(User.getDefaultImagePath(getDatabase()));
+			
+			t.set_var( "options.message", "Pfad zum Grafikpak zur&uuml;ckgesetzt<br />\n" );
+		} 
+		else {
+			if( (gfxpak.indexOf('/') == -1) && (gfxpak.indexOf('\\') != -1) ) {
+				gfxpak = StringUtils.replace(gfxpak, "\\", "/");
+			} 
+			if( !gfxpak.endsWith("/") ){
+				gfxpak += '/';
+			}
+			if( gfxpak.indexOf("://") == -1 ) {
+				if( !gfxpak.startsWith("/") ) {
+					gfxpak = '/'+gfxpak;
+				}
+				gfxpak = "file://"+gfxpak;
+			}
+			user.setImagePath( gfxpak );
+			
+			t.set_var( "options.message", "Pfad zum Grafikpak ge&auml;ndert<br />\n" );
+		}
+		
+		redirect();
 	}
 	
 	/**
@@ -188,8 +293,49 @@ public class OptionsController extends DSGenerator {
 	 *
 	 */
 	public void saveOptionsAction() {
-		// TODO
-		Common.stub();
+		Database db = getDatabase();
+		User user = getUser();
+		TemplateEngine t = getTemplateEngine();
+		
+		parameterNumber("enableipsess");
+		parameterNumber("enableautologout");
+		parameterNumber("showtooltip");
+		parameterNumber("wrapfactor");
+		
+		boolean enableipsess = getInteger("enableipsess") != 0 ? true : false;
+		boolean enableautologout = getInteger("enableautologout") != 0 ? true : false;
+		boolean showtooltip = getInteger("showtooltip") != 0 ? true : false;
+		int wrapfactor = getInteger("wrapfactor");
+	
+		String changemsg = "";
+
+		if( enableipsess == user.hasFlag( User.FLAG_DISABLE_IP_SESSIONS ) ) {
+			user.setFlag( User.FLAG_DISABLE_IP_SESSIONS, !enableipsess );
+			
+			changemsg += "Session-ID von der IP-Adresse "+(enableipsess ? "ge" : "ent" )+"koppelt<br />\n";
+		} 
+	
+		if( enableautologout == user.hasFlag( User.FLAG_DISABLE_AUTO_LOGOUT ) ) {
+			user.setFlag( User.FLAG_DISABLE_AUTO_LOGOUT, !enableautologout );
+		 
+			changemsg += "Das automatische Ausloggen wurde "+(enableautologout ? "" : "de" )+"aktiviert<br />\n";
+		} 
+
+		if( showtooltip != (Integer.parseInt(user.getUserValue("TBLORDER/schiff/tooltips")) != 0) ) {
+			user.setUserValue( "TBLORDER/schiff/tooltips", showtooltip ? "1" : "0" );
+			
+			changemsg += "Anzeige der Tooltips "+(showtooltip ? "" : "de")+"aktiviert<br />\n";		
+		} 
+	
+		if( wrapfactor != Integer.parseInt(user.getUserValue("TBLORDER/schiff/wrapfactor")) ) {
+			user.setUserValue("TBLORDER/schiff/wrapfactor", Integer.toString(wrapfactor) );
+			
+			changemsg += "Schiffsgruppierungen "+(wrapfactor != 0 ? "aktiviert" : "deaktiviert")+"<br />\n";
+		}
+	
+		t.set_var( "options.message", changemsg );
+	
+		redirect();
 	}
 	
 	/**
