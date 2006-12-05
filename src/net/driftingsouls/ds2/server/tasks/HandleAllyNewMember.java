75d7c220a5e0f4ec5567139ff120785f47d938dc
Christopher Jung <bktheg@web.de>
2006-12-05 16:24:58 +0100

    Handler fuer Task 'AllyNewMember' eingebaut
    committer: Christopher Jung <bktheg@web.de>

--- src/net/driftingsouls/ds2/server/tasks/HandleAllyNewMember.java
+++ src/net/driftingsouls/ds2/server/tasks/HandleAllyNewMember.java
@@ -18,7 +18,17 @@
  */
 package net.driftingsouls.ds2.server.tasks;
 
+import org.apache.commons.lang.StringUtils;
+
+import net.driftingsouls.ds2.server.ContextCommon;
+import net.driftingsouls.ds2.server.comm.PM;
 import net.driftingsouls.ds2.server.framework.Common;
+import net.driftingsouls.ds2.server.framework.Context;
+import net.driftingsouls.ds2.server.framework.ContextMap;
+import net.driftingsouls.ds2.server.framework.User;
+import net.driftingsouls.ds2.server.framework.UserIterator;
+import net.driftingsouls.ds2.server.framework.db.Database;
+import net.driftingsouls.ds2.server.framework.db.SQLResultRow;
 
 /**
  * TASK_ALLY_NEW_MEMBER
@@ -33,8 +43,54 @@ import net.driftingsouls.ds2.server.framework.Common;
 class HandleAllyNewMember implements TaskHandler {
 
 	public void handleEvent(Task task, String event) {	
-		// TODO
-		Common.stub();
+		Context context = ContextMap.getContext();
+		User user = context.getActiveUser();
+		Database db = context.getDatabase();
+		
+		int playerID = Integer.parseInt(task.getData2());
+		
+		if( event.equals("pm_yes") ) {
+			SQLResultRow ally = db.first("SELECT id,name,allytag FROM ally WHERE id=",task.getData1());
+			
+			User player = context.createUserObject(playerID);
+			String newname = ally.getString("allytag");
+			newname = StringUtils.replace(newname, "[name]", player.getNickname());
+			player.setAlly(ally.getInt("id"));
+			player.setName(newname);
+			
+			int tick = context.get(ContextCommon.class).getTick();
+			player.addHistory(Common.getIngameTime(tick)+": Beitritt zur Allianz "+ally.getString("name"));
+			
+			int membercount = 1;
+			
+			// Beziehungen auf "Freund" setzen
+			UserIterator iter = context.createUserIterator("SELECT * FROM users WHERE ally=",ally.getInt("id")," AND id!=",player.getID());
+			for( User allymember : iter ) {
+				allymember.setRelation(player.getID(), User.Relation.FRIEND);
+				player.setRelation(allymember.getID(), User.Relation.FRIEND);
+				
+				membercount++;
+			}
+			iter.free();
+			
+			PM.send( context, user.getID(), player.getID(), "Aufnahmeantrag", "[Automatische Nachricht]\nDu wurdest in die Allianz >"+ally.getString("name")+"< aufgenommen\n\nHerzlichen Gr&uuml;ckwunsch!");
+			
+			// Check, ob wir eine TM_TASK_LOW_MEMBER entfernen muessen
+			if( membercount == 3 ) {
+				Task[] tasks = Taskmanager.getInstance().getTasksByData( Taskmanager.Types.ALLY_LOW_MEMBER, Integer.toString(ally.getInt("id")), "*", "*" );
+				for( int i=0; i < tasks.length; i++ ) {
+					Taskmanager.getInstance().removeTask( tasks[i].getTaskID() );
+				}
+			}
+		}
+		else if( event.equals("pm_no") ) {
+			PM.send( context, 0, playerID, "Aufnahmeantrag", "[Automatische Nachricht]\nDein Antrag wurde leider abgelehnt. Es steht dir nun frei ob du einen neuen Antrag nach absprache mit der Allianz stellen willst oder ob du dich an eine andere Allianz wendest.");
+		}
+		else if( event.equals("tick_timeout") ) {
+			PM.send( context, 0, playerID, "Aufnahmeantrag", "[Automatische Nachricht]\nDein Antrag wurde leider nicht innerhalb der vorgegebenen Zeit bearbeitet und daher entfernt. Du hast jedoch jederzeit die M&ouml;glichkeit einen neuen Antrag zu stellen.");
+		}
+		
+		Taskmanager.getInstance().removeTask( task.getTaskID() );
 	}
 
 }
