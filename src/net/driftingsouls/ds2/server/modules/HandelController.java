c3ace648c16d41756c1c24c841d1cd6a45aab3ab
Christopher Jung <bktheg@web.de>
2006-12-27 18:56:12 +0100

    Modul 'Handel' eingebaut
    committer: Christopher Jung <bktheg@web.de>

---
+++ src/net/driftingsouls/ds2/server/modules/HandelController.java
@@ -0,0 +1,280 @@
+/*
+ *	Drifting Souls 2
+ *	Copyright (c) 2006 Christopher Jung
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation; either
+ *	version 2.1 of the License, or (at your option) any later version.
+ *
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package net.driftingsouls.ds2.server.modules;
+
+import net.driftingsouls.ds2.server.cargo.Cargo;
+import net.driftingsouls.ds2.server.cargo.ResourceEntry;
+import net.driftingsouls.ds2.server.cargo.ResourceList;
+import net.driftingsouls.ds2.server.cargo.Resources;
+import net.driftingsouls.ds2.server.cargo.WarenID;
+import net.driftingsouls.ds2.server.config.Items;
+import net.driftingsouls.ds2.server.config.ResourceConfig;
+import net.driftingsouls.ds2.server.framework.Common;
+import net.driftingsouls.ds2.server.framework.Configuration;
+import net.driftingsouls.ds2.server.framework.Context;
+import net.driftingsouls.ds2.server.framework.User;
+import net.driftingsouls.ds2.server.framework.db.Database;
+import net.driftingsouls.ds2.server.framework.db.SQLQuery;
+import net.driftingsouls.ds2.server.framework.db.SQLResultRow;
+import net.driftingsouls.ds2.server.framework.pipeline.generators.DSGenerator;
+import net.driftingsouls.ds2.server.framework.templates.TemplateEngine;
+
+/**
+ * Zeigt aktive Handelsangebote an und ermoeglicht das Erstellen eigener Handelsangebote
+ * @author Christopher Jung
+ *
+ */
+public class HandelController extends DSGenerator {
+
+	/**
+	 * Konstruktor
+	 * @param context Der zu verwendende Kontext
+	 */
+	public HandelController(Context context) {
+		super(context);
+		
+		setTemplate("handel.html");
+	}
+	
+	@Override
+	protected boolean validateAndPrepare(String action) {
+		return true;
+	}
+
+	/**
+	 * Speichert ein neues Handelsangebot in der Datenbank
+	 * @urlparam String comm Die Beschreibung
+	 * @urlparam Integer ($warenid|"i"+$itemid)+"need" Benoetigte Waren
+	 * @urlparam Integer ($warenid|"i"+$itemid)+"have" Angebotene Waren
+	 *
+	 */
+	public void enterAction() {
+		Database db = getDatabase();
+		
+		parameterString("comm");
+		String comm = getString("comm");
+		
+		boolean needeverything = false;
+		boolean haveeverything = false;
+		Cargo need = new Cargo();
+		Cargo have = new Cargo();
+
+	
+		// Egal - "-1" (Spezialfall)
+		parameterNumber("-1need");			
+		long needcount = getInteger("-1need");
+		long havecount = 0;
+		
+		if( needcount <= 0 ) {
+			parameterNumber("-1have");
+			havecount = getInteger("-1have");
+			
+			if( havecount > 0 ) {
+				haveeverything = true; 
+			}
+		}
+		else {
+			needeverything = true;
+		}
+		
+		
+		ResourceList reslist = Resources.RESOURCE_LIST.getResourceList();
+		for( ResourceEntry res : reslist ) {
+			String name = "";
+			
+			if( res.getId().isItem() ) {
+				if( !Items.get().item(res.getId().getItemID()).getHandel() ) {
+					continue;
+				}
+				name = "i"+res.getId().getID();
+			}
+			else {
+				if( ResourceConfig.getResourceHidden(res.getId().getID()) ) {
+					continue;
+				}
+				name = Integer.toString(res.getId().getID());
+			}
+					
+			parameterNumber(name+"need");			
+			needcount = getInteger(name+"need");
+			havecount = 0;
+			
+			if( needcount <= 0 ) {
+				parameterNumber(name+"have");
+				havecount = getInteger(name+"have");
+			}
+						
+			if( needcount > 0 ) {
+				need.addResource(res.getId(), needcount);
+			}
+			if( havecount > 0 ) {
+				have.addResource(res.getId(), havecount);
+			}
+		}	
+		
+		String needStr = "-1";
+		if( !needeverything ) {
+			needStr = need.save();	
+		}
+		
+		String haveStr = "-1";
+		if( !haveeverything ) {
+			haveStr = have.save();	
+		}
+
+		db.prepare("INSERT INTO handel " ,
+				"VALUES (0, ?, ?, ?, ?, ?)")
+			.update(getUser().getID(), Common.time(), needStr, haveStr, comm);
+
+		redirect();
+	}
+	
+	/**
+	 * Zeigt die Seite zur Eingabe eines Handelsangebots an 
+	 *
+	 */
+	public void addAction() {
+		TemplateEngine t = getTemplateEngine();
+		
+		t.set_var("handel.add", 1);
+		
+		t.set_block("_HANDEL", "addresources.listitem", "addresources.list");
+		
+		ResourceList reslist = Resources.RESOURCE_LIST.getResourceList();
+		for( ResourceEntry res : reslist ) {
+			if( res.getId().isItem() ) {
+				if( !Items.get().item(res.getId().getItemID()).getHandel() ) {
+					continue;
+				}
+			}
+			else {
+				if( ResourceConfig.getResourceHidden(res.getId().getID()) ) {
+					continue;
+				}
+			}
+			
+			t.set_var(	"res.id",		(res.getId().isItem() ? "i"+res.getId().getItemID() : res.getId().getID()),
+						"res.name",		res.getName(),
+						"res.image",	res.getImage() );
+			
+			t.parse("addresources.list", "addresources.listitem", true);
+		}
+	}
+
+	/**
+	 * Loescht ein Handelsangebot
+	 * @urlparam Integer del Die ID des zu loeschenden Handelsangebots
+	 *
+	 */
+	public void deleteAction() {
+		User user = getUser();
+		TemplateEngine t = getTemplateEngine();
+		Database db = getDatabase();
+		
+		parameterNumber("del");
+		int del = getInteger("del");
+		
+		SQLResultRow line = db.first("SELECT who FROM handel WHERE id=",del);
+		if( !line.isEmpty() && ((line.getInt("who") == user.getID()) || (user.getAccessLevel() >= 20) || user.hasFlag(User.FLAG_MODERATOR_HANDEL)) ) {
+			db.update("DELETE FROM handel WHERE id=",del);
+			t.set_var("handel.message", "Angebot gel&ouml;scht");
+		} 
+		else {
+			addError("Sie haben keine Berechtigung das Angebot zu l&ouml;schen");
+		}
+		
+		redirect();
+	}
+	
+	/**
+	 * Zeigt die vorhandenen Handelsangebote an
+	 */
+	@Override
+	public void defaultAction() {		
+		Database db = getDatabase();
+		TemplateEngine t = getTemplateEngine();
+		User user = getUser();
+		
+		t.set_var("handel.view", 1);
+		
+		int count = 0;
+		
+		t.set_block("_HANDEL", "angebote.listitem", "angebote.list");
+		t.set_block("angebote.listitem", "angebot.want.listitem", "angebot.want.list");
+		t.set_block("angebote.listitem", "angebot.need.listitem", "angebot.need.list");
+		
+		SQLQuery act = db.query("SELECT handel.* FROM handel JOIN users ON handel.who=users.id WHERE users.vaccount=0 OR users.wait4vac!=0 ORDER BY handel.time DESC");
+		while( act.next() ) {
+			User auser = getContext().createUserObject(act.getInt("who"));
+		
+			t.set_var(	"angebot.want.list",	"",
+						"angebot.need.list",	"" );
+			
+			for( int i = 0; i <= 1; i++ ) {
+				String line = (i == 1 ? act.getString("bietet") : act.getString("sucht"));
+				
+				if( !line.equals("-1") ) {
+					Cargo cargo = new Cargo( Cargo.Type.STRING, line );
+					cargo.setOption( Cargo.Option.SHOWMASS, false );
+					cargo.setOption( Cargo.Option.LINKCLASS, "handelwaren");
+					
+					ResourceList reslist = cargo.getResourceList();
+					if( i == 0 ) {
+						Resources.echoResList( t, reslist, "angebot.want.list");
+					}
+					else {
+						Resources.echoResList( t, reslist, "angebot.need.list");
+					}
+				}
+				else {
+					t.set_var(	"res.cargo",	1,
+								"res.id",		-1,
+								"res.image",	Configuration.getSetting("URL")+"data/interface/handel/open.gif");
+					if( i == 0 ) {
+						t.parse("angebot.want.list", "angebot.want.listitem", true);
+					}
+					else {
+						t.parse("angebot.need.list", "angebot.need.listitem", true);
+					}
+				}
+			}
+			
+			t.set_var(	"angebot.id",			act.getInt("id"),
+						"angebot.owner",		act.getInt("who"),
+						"angebot.owner.name",	Common._title(auser.getName()),
+						"angebot.date",			Common.date("d.m.Y H:i:s",act.getLong("time")),
+						"angebot.description",	Common._text(act.getString("comm")),
+						"angebot.description.overflow",	Common._text(act.getString("comm")).length() > 220,
+						"angebot.newline",		(count % 3 == 0),
+						"angebot.endline",		(count % 3 == 0) && (count > 0),
+						"angebot.showdelete",	(act.getInt("who") == user.getID()) || (user.getAccessLevel() >= 20) || user.hasFlag(User.FLAG_MODERATOR_HANDEL) );
+
+			count++;
+
+			t.parse("angebote.list","angebote.listitem", true);
+		}
+		act.free();
+		
+		t.set_block("_HANDEL", "emptyangebote.listitem", "emptyangebote.list");
+		while( count % 3 != 0 ) {
+			t.parse("emptyangebote.list", "emptyangebote.listitem", true);
+			count++;
+		}	
+	}
+}
