1d7c89f9259d4be911e7273ea4238ebf9ab4775d
Christopher Jung <bktheg@web.de>
2006-12-29 13:09:10 +0100

    Ein paar Fixes
    committer: Christopher Jung <bktheg@web.de>

--- src/net/driftingsouls/ds2/server/framework/bbcode/TagResource.java
+++ src/net/driftingsouls/ds2/server/framework/bbcode/TagResource.java
@@ -35,71 +35,80 @@ class TagResource implements BBCodeFunction {
 	public String handleMatch(String content, String... values) {
 		Context context = ContextMap.getContext();
 		
-		long count = Long.parseLong(content);
-		ResourceID rid = Resources.fromString(values[0]);
-		String format = "in";
-		if( values.length > 1 ) {
-			format = values[1];
-		}
-		
-		
-		if( rid.isItem() ) {			
-			String unknstr = "Unbekannter Gegenstand";
+		try {
+			long count = 0;
+			if( content.length() > 0 ) {
+				count = Long.parseLong(content);
+			}
+			ResourceID rid = Resources.fromString(values[0]);
+			String format = "in";
+			if( values.length > 1 ) {
+				format = values[1];
+			}
+			
+			
+			if( rid.isItem() ) {			
+				String unknstr = "Unbekannter Gegenstand";
+				if( count != 0 ) {
+					unknstr = Common.ln(count)+"x "+unknstr;
+				}
+				
+				Item item = Items.get().item(rid.getItemID());
+				
+				if( item == null ) {
+					return unknstr;	
+				}
+				
+				User user = context.getActiveUser();
+				if( (user == null) && (item.getAccessLevel() > 0 || item.isUnknownItem() )) {
+					return unknstr;
+				}
+				else if( user != null ){
+					if( item.getAccessLevel() > user.getAccessLevel() ) {		
+						return unknstr;
+					}
+				
+					if( item.isUnknownItem() && !user.isKnownItem(item.getID()) && (user.getAccessLevel() < 15) ) {
+						return unknstr;
+					}
+				}	
+			}
+	
+			Cargo cargo = new Cargo();
 			if( count != 0 ) {
-				unknstr = Common.ln(count)+"x "+unknstr;
+				cargo.addResource(rid, count);
+			}
+			else {
+				cargo.addResource(rid,1);
+			}
+			cargo.setOption( Cargo.Option.SHOWMASS, false );
+	
+			ResourceList reslist = cargo.getResourceList();
+			ResourceEntry res = reslist.iterator().next();
+			
+			StringBuilder tmpString = new StringBuilder(30);
+	
+			if( count != 0 ) {
+				tmpString.append(Common.ln(count));
+				tmpString.append("x ");
+			}
+	
+			if( format.indexOf('i') != -1 ) {
+				tmpString.append("<img align=\"middle\" border=\"0\" src=\"");
+				tmpString.append(res.getImage());
+				tmpString.append("\" alt=\"\" />");
+			}
+	
+			if( format.indexOf('n') != -1 ) {
+				tmpString.append(res.getName());
 			}
 			
-			Item item = Items.get().item(rid.getItemID());
-			
-			if( item == null ) {
-				return unknstr;	
-			}
-			
-			User user = context.getActiveUser();
-			if( (user == null) && (item.getAccessLevel() > 0 || item.isUnknownItem() )) {
-				return unknstr;
-			}
-			else if( user != null ){
-				if( item.getAccessLevel() > user.getAccessLevel() ) {		
-					return unknstr;
-				}
-			
-				if( item.isUnknownItem() && !user.isKnownItem(item.getID()) && (user.getAccessLevel() < 15) ) {
-					return unknstr;
-				}
-			}	
-		}
 
-		Cargo cargo = new Cargo();
-		if( count != 0 ) {
-			cargo.addResource(rid, count);
+			return tmpString.toString();
 		}
-		else {
-			cargo.addResource(rid,1);
+		catch( NumberFormatException e ) {
+			return "<span style=\"color:red\">Ungueltiger Resourcen-Tag: [resource="+Common.implode(",", values)+"]"+content+"[/resource]</span>";
 		}
-		cargo.setOption( Cargo.Option.SHOWMASS, false );
-
-		ResourceList reslist = cargo.getResourceList();
-		ResourceEntry res = reslist.next();
-		
-		StringBuilder tmpString = new StringBuilder(30);
-
-		if( count != 0 ) {
-			tmpString.append(Common.ln(count));
-			tmpString.append("x ");
-		}
-
-		if( format.indexOf('i') != -1 ) {
-			tmpString.append("<img align=\"middle\" border=\"0\" src=\"");
-			tmpString.append(res.getImage());
-			tmpString.append("\" alt=\"\" />");
-		}
-
-		if( format.indexOf('n') != -1 ) {
-			tmpString.append(res.getName());
-		}
-
-	 	return tmpString.toString();
 	}
 
 }
