41ee9f08cbd64dd57210ad3c25095945fa32c5ea
Christopher Jung <bktheg@web.de>
2007-01-13 10:48:13 +0100

    Erster Teil NPC-GUI eingebaut
    committer: Christopher Jung <bktheg@web.de>

---
+++ src/net/driftingsouls/ds2/server/modules/NPCOrderController.java
@@ -0,0 +1,135 @@
+package net.driftingsouls.ds2.server.modules;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import net.driftingsouls.ds2.server.config.Faction;
+import net.driftingsouls.ds2.server.config.Rassen;
+import net.driftingsouls.ds2.server.framework.Common;
+import net.driftingsouls.ds2.server.framework.Context;
+import net.driftingsouls.ds2.server.framework.User;
+import net.driftingsouls.ds2.server.framework.db.Database;
+import net.driftingsouls.ds2.server.framework.db.SQLQuery;
+import net.driftingsouls.ds2.server.framework.pipeline.generators.DSGenerator;
+import net.driftingsouls.ds2.server.framework.templates.TemplateEngine;
+import net.driftingsouls.ds2.server.ships.Ships;
+
+/**
+ * Das Interface fuer NPCs
+ * @author Christopher Jung
+ *
+ */
+public class NPCOrderController extends DSGenerator {
+	private boolean isHead = false;
+	
+	/**
+	 * Konstruktor
+	 * @param context Der zu verwendende Kontext
+	 */
+	public NPCOrderController(Context context) {
+		super(context);
+		
+		setTemplate("npcorder.html");	
+	}
+	
+	@Override
+	protected boolean validateAndPrepare(String action) {
+		User user = this.getUser();
+		TemplateEngine t = this.getTemplateEngine();
+		
+		if( !user.hasFlag( User.FLAG_ORDER_MENU ) ) {
+			addError("Nur NPCs k&ouml;nnen dieses Script nutzen", Common.buildUrl(getContext(), "default", "module", "ueber") );
+			
+			return false;
+		}
+		
+		if( Rassen.get().rasse(user.getRace()).isHead(user.getID()) ) {
+			t.set_var( "npcorder.factionhead", 1 );
+								
+			this.isHead = true;	
+		}
+		
+		if( Faction.get(user.getID()) != null && Faction.get(user.getID()).getPages().hasPage("shop") ) {
+			t.set_var("npcorder.shop", 1);	
+		}
+		
+		return true;
+	}
+
+	/**
+	 * Zeigt die GUI zum Ordern von Schiffen/Offizieren
+	 */
+	@Override
+	public void defaultAction() {
+		Database db = getDatabase();
+		TemplateEngine t = this.getTemplateEngine();
+		User user = this.getUser();
+		
+		Map<Integer,Integer> orders = new HashMap<Integer,Integer>();
+		
+		t.set_var( "npcorder.ordermenu", 1 );
+
+		SQLQuery order = db.query("SELECT type FROM orders WHERE user=",user.getID());
+		while( order.next() ) {
+			Common.safeIntInc(orders, order.getInt("type"));
+		}
+		order.free();
+
+		/*
+			Schiffe
+		*/
+		
+		int oldclass = 0;
+
+		t.set_block("_NPCORDER", "ships.listitem", "ships.list");
+
+		SQLQuery ship = db.query("SELECT t1.*,t2.nickname name,t2.class FROM orders_ships t1 JOIN ship_types t2 ON t1.type=t2.id ORDER BY t2.class,t1.type");
+		while( ship.next() ) {
+			t.start_record();
+			
+			if( ship.getInt("class") != oldclass ) {
+				t.set_var(	"ship.newclass",		1,
+							"ship.newclass.name",	Ships.getShipClass(ship.getInt("class")).getSingular() );
+				
+				oldclass = ship.getInt("class");
+			}
+			
+			if( orders.containsKey(ship.getInt("type")) ) {
+				orders.put(ship.getInt("type"), 0);
+			}
+			
+			t.set_var(	"ship.name",		ship.getString("name"),
+						"ship.type",		ship.getInt("type"),
+						"ship.cost",		ship.getInt("cost"),
+						"ship.ordercount",	orders.get(ship.getInt("type")) );
+								
+			t.parse("ships.list", "ships.listitem", true);
+			
+			t.stop_record();
+			t.clear_record();
+		}
+		ship.free();
+		
+		/*
+			Offiziere
+		*/
+		
+		t.set_block("_NPCORDER", "offiziere.listitem", "offiziere.list");
+		
+		SQLQuery offizier = db.query("SELECT * FROM orders_offiziere WHERE cost > 0 ORDER BY id");
+		while( offizier.next() ) {
+			if( orders.containsKey(-offizier.getInt("id")) ) {
+				orders.put(-offizier.getInt("id"), 0);
+			}
+			
+			t.set_var(	"offizier.name",		offizier.getString("name"),
+						"offizier.rang",		offizier.getInt("rang"),
+						"offizier.cost",		offizier.getInt("cost"),
+						"offizier.id",			-offizier.getInt("id"),
+						"offizier.ordercount",	orders.get(offizier.getInt("id")) );
+								
+			t.parse("offiziere.list", "offiziere.listitem", true);
+		}
+		offizier.free();
+	}
+}
