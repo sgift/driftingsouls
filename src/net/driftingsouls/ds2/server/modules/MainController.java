8db7ad0cfc1db64df3e1af114f8bb21a6af5b514
Christopher Jung <bktheg@web.de>
2007-01-15 22:40:28 +0100

    Die Admin-Konsole wird nun korrekt eingeblendet
    committer: Christopher Jung <bktheg@web.de>

--- src/net/driftingsouls/ds2/server/modules/MainController.java
+++ src/net/driftingsouls/ds2/server/modules/MainController.java
@@ -88,7 +88,7 @@ public class MainController extends DSGenerator {
 			out.append("<frame src=\"./main.php?sess="+getString("sess")+"&amp;module=links\" id=\"_framenavi\" name=\"navi\" frameborder=\"0\" />\n");
 			out.append("<frameset rows=\"*,75\" framespacing=\"0\" border=\"0\" frameborder=\"0\">\n");
 			out.append("<frame src=\"./main.php?sess="+getString("sess")+"&amp;module=ueber\" id=\"_framemain\" name=\"main\" scrolling=\"auto\" frameborder=\"0\" />\n");
-			out.append("<frame src=\"./main.php?sess="+getString("sess")+"&amp;module=admin&amp;namedplugin=adminConsole&amp;cleanpage=1\" id=\"_frameconsole\" name=\"console\" scrolling=\"auto\" frameborder=\"0\" />\n");
+			out.append("<frame src=\"./main.php?sess="+getString("sess")+"&amp;module=admin&amp;namedplugin=net.driftingsouls.ds2.server.modules.admin.AdminConsole&amp;cleanpage=1\" id=\"_frameconsole\" name=\"console\" scrolling=\"auto\" frameborder=\"0\" />\n");
 			out.append("</frameset>\n");
 			out.append("</frameset>\n");
 		}
