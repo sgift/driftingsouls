20c88fdd28ed1066c2b555e40645a406a63781d6
Christopher Jung <bktheg@web.de>
2007-01-15 13:42:55 +0100

    Statistiken 'BiggestFleet' und 'MemberCount' auf die neue Basisklasse portiert
    committer: Christopher Jung <bktheg@web.de>

--- src/net/driftingsouls/ds2/server/modules/stats/StatBiggestFleet.java
+++ src/net/driftingsouls/ds2/server/modules/stats/StatBiggestFleet.java
@@ -18,7 +18,6 @@
  */
 package net.driftingsouls.ds2.server.modules.stats;
 
-import net.driftingsouls.ds2.server.framework.Common;
 import net.driftingsouls.ds2.server.framework.Context;
 import net.driftingsouls.ds2.server.framework.ContextMap;
 import net.driftingsouls.ds2.server.framework.db.Database;
@@ -30,7 +29,7 @@ import net.driftingsouls.ds2.server.modules.StatsController;
  * @author Christopher Jung
  *
  */
-public class StatBiggestFleet implements Statistic {
+public class StatBiggestFleet extends AbstractStatistic implements Statistic {
 	private boolean allys;
 
 	/**
@@ -45,42 +44,30 @@ public class StatBiggestFleet implements Statistic {
 		Context context = ContextMap.getContext();
 		Database db = context.getDatabase();
 
-		StringBuffer echo = context.getResponse().getContent();
-		String url = "";
+		String url = null;
 		
 		SQLQuery tmp = null;
 		if( !allys ) {
-			tmp = db.query("SELECT count( t1.id ) ships,t2.name,t2.id ",
+			tmp = db.query("SELECT count( t1.id ) count,t2.name,t2.id ",
 					    "FROM ((ships t1 JOIN users t2 ON t1.owner=t2.id) JOIN ship_types t3 ON t1.type=t3.id) LEFT OUTER JOIN ships_modules t4 ON t1.id=t4.id ",
 						"WHERE t1.id>0 AND t1.owner>",StatsController.MIN_USER_ID," AND (((t4.id IS NULL) AND t3.cost>0) OR ((t4.id IS NOT NULL) AND t4.cost>0))",
 						"GROUP BY t1.owner ",
-						"ORDER BY ships DESC,t2.id ASC LIMIT ",size);
-			url = "./main.php?module=allylist&amp;sess="+context.getSession()+"&amp;details=";
+						"ORDER BY count DESC,t2.id ASC LIMIT ",size);
+			url = getUserURL();
 		}
 		else {
-			tmp = db.query("SELECT count( s.id ) ships,a.name,u.ally id ",
+			tmp = db.query("SELECT count( s.id ) count,a.name,u.ally id ",
 					    "FROM (((ships s JOIN users u ON s.owner=u.id) JOIN ship_types st ON s.type=st.id) JOIN ally a ON u.ally=a.id) LEFT OUTER JOIN ships_modules sm ON s.id=sm.id ",
 						"WHERE s.id>0 AND s.owner>",StatsController.MIN_USER_ID," AND u.ally>0 AND (((sm.id IS NULL) AND st.cost>0) OR ((sm.id IS NOT NULL) AND sm.cost>0)) ",
 						"GROUP BY u.ally ",
-						"ORDER BY ships DESC,u.id ASC LIMIT ",size);
+						"ORDER BY count DESC,u.id ASC LIMIT ",size);
 		
-			url = "./main.php?module=allylist&amp;sess="+context.getSession()+"&amp;action=details&amp;details=";
+			url = getAllyURL();
 		}
 	
-		echo.append("<table class=\"noBorderX\" cellspacing=\"1\" cellpadding=\"1\" width=\"100%\">\n");
-		echo.append("<tr><td class=\"noBorderX\" colspan=\"4\" align=\"left\">Die groessten Flotten (nur Schiffe!):</td></tr>\n");
-	
-		int count = 0;
-		while( tmp.next() ) {
-	   		echo.append("<tr><td class=\"noBorderX\" style=\"width:40px\">"+(count+1)+".</td>\n");
-			echo.append("<td class=\"noBorderX\"><a class=\"profile\" href=\""+url+tmp.getInt("id")+"\">"+Common._title(tmp.getString("name"))+" ("+tmp.getInt("id")+")</a></td>\n");
-			echo.append("<td class=\"noBorderX\">&nbsp;-&nbsp;</td>\n");
-			echo.append("<td class=\"noBorderX\">"+Common.ln(tmp.getInt("ships"))+"</td></tr>\n");
-	   		
-	   		count++;
-		}
+		this.generateStatistic("Die groessten Flotten (nur Schiffe!):", tmp, url);
+
 		tmp.free();
-		echo.append("</table><br /><br />\n");
 	}
 
 }
--- src/net/driftingsouls/ds2/server/modules/stats/StatMemberCount.java
+++ src/net/driftingsouls/ds2/server/modules/stats/StatMemberCount.java
@@ -18,7 +18,6 @@
  */
 package net.driftingsouls.ds2.server.modules.stats;
 
-import net.driftingsouls.ds2.server.framework.Common;
 import net.driftingsouls.ds2.server.framework.Context;
 import net.driftingsouls.ds2.server.framework.ContextMap;
 import net.driftingsouls.ds2.server.framework.db.Database;
@@ -30,30 +29,22 @@ import net.driftingsouls.ds2.server.modules.StatsController;
  * @author Christopher Jung
  *
  */
-public class StatMemberCount implements Statistic {
+public class StatMemberCount extends AbstractStatistic implements Statistic {
 
 	public void show(StatsController contr, int size) {
 		Context context = ContextMap.getContext();
 		Database db = context.getDatabase();
 
-		StringBuffer echo = context.getResponse().getContent();
+		String url = getAllyURL();
 		
-		String url = "./main.php?module=allylist&amp;sess="+context.getSession()+"&amp;action=details&amp;details=";
+		SQLQuery tmp = db.query("SELECT a.id, a.name, count(u.id) count " +
+				"FROM ally a JOIN users u ON a.id=u.ally " +
+				"WHERE a.id>",StatsController.MIN_USER_ID," " +
+				"GROUP BY a.id ORDER BY count DESC LIMIT "+size);
 	
-		echo.append("<table class=\"noBorderX\" cellspacing=\"1\" cellpadding=\"1\" width=\"100%\">\n");
-		echo.append("<tr><td class=\"noBorderX\" colspan=\"4\" align=\"left\">Die gr&ouml;&szlig;ten Allianzen:</td></tr>\n");
+		this.generateStatistic("Die gr&ouml;&szlig;ten Allianzen:", tmp, url);
 		
-		int count = 0;
-		
-		SQLQuery tmp = db.query("SELECT a.id, a.name, count(u.id) membercount FROM ally a JOIN users u ON a.id=u.ally WHERE a.id>",StatsController.MIN_USER_ID," GROUP BY a.id ORDER BY membercount DESC LIMIT "+size);
-		while( tmp.next() ) {
-	   		echo.append("<tr><td class=\"noBorderX\" style=\"width:40px\">"+(count+1)+".</td>\n");
-			echo.append("<td class=\"noBorderX\"><a class=\"profile\" href=\""+url+tmp.getInt("id")+"\">"+Common._title(tmp.getString("name"))+" ("+tmp.getInt("id")+")</td>");
-			echo.append("<td class=\"noBorderX\">&nbsp;-&nbsp;</td>\n");
-			echo.append("<td class=\"noBorderX\">"+Common.ln(tmp.getInt("membercount"))+"</td></tr>\n");
-		}
 		tmp.free();
-		echo.append("</table><br /><br />\n");
 	}
 
 }
