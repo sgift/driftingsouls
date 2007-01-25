1756df70fafba366497c0a2546d3de5daf9c395e
Christopher Jung <bktheg@web.de>
2007-01-25 12:52:54 +0100

    @member-of gefixt
    committer: Christopher Jung <bktheg@web.de>

--- src/net/driftingsouls/ds2/server/config/Rassen.java
+++ src/net/driftingsouls/ds2/server/config/Rassen.java
@@ -85,8 +85,8 @@ public class Rassen implements Iterable<Rasse>, Loggable {
 				boolean playable = "true".equals(XMLUtils.getStringByXPath(node, "@playable"));
 				boolean extPlayable = "true".equals(XMLUtils.getStringByXPath(node, "@ext-playable"));
 				int memberIn = -1;
-				if( XMLUtils.getNodeByXPath(node, "member-of") != null ) {
-					memberIn = XMLUtils.getNumberByXPath(node, "member-of").intValue();
+				if( XMLUtils.getNodeByXPath(node, "@member-of") != null ) {
+					memberIn = XMLUtils.getNumberByXPath(node, "@member-of").intValue();
 				}
 				
 				String name = XMLUtils.getStringByXPath(node, "name/text()");
