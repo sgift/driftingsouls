afcd478e0014e0ed60a6e5b2382f531b3da2048f
Christopher Jung <bktheg@web.de>
2007-02-25 10:48:45 +0100

    Kommentare hinzugefuegt
    committer: Christopher Jung <bktheg@web.de>

--- src/net/driftingsouls/ds2/server/ships/ShipClasses.java
+++ src/net/driftingsouls/ds2/server/ships/ShipClasses.java
@@ -24,26 +24,89 @@ package net.driftingsouls.ds2.server.ships;
  *
  */
 public enum ShipClasses {
+	/**
+	 * Die Schiffsklasse "Unbekannt"
+	 */
 	UNBEKANNT("Unbekannt"),						// 0
+	/**
+	 * Die Schiffsklasse "Transporter"
+	 */
 	TRANSPORTER("Transporter"),					// 1
+	/**
+	 * Die Schiffsklasse "Zerstoerer"
+	 */
 	ZERSTOERER("Zerst&ouml;rer"),				// 2
+	/**
+	 * Die Schiffsklasse "Tanker"
+	 */
 	TANKER("Tanker"),							// 3
+	/**
+	 * Die Schiffsklasse "Juggernauten"
+	 */
 	JUGGERNAUT("Juggernaut","Juggernauten"),	// 4
+	/**
+	 * Die Schiffsklasse "Korvetten"
+	 */
 	KORVETTE("Korvette", "Korvetten"),			// 5
+	/**
+	 * Die Schiffsklasse "Kreuzer"
+	 */
 	KREUZER("Kreuzer", "Kreuzer"),				// 6
+	/**
+	 * Die Schiffsklasse "Schwere Kreuzer"
+	 */
 	SCHWERER_KREUZER("Schwerer Kreuzer", "Schwere Kreuzer"),	// 7
+	/**
+	 * Die Schiffsklasse "Stationen"
+	 */
 	STATION("Station", "Stationen"),			// 8
+	/**
+	 * Die Schiffsklasse "Jaeger"
+	 */
 	JAEGER("J&auml;ger"),						// 9
+	/**
+	 * Die Schiffsklasse "Geschuetze"
+	 */
 	GESCHUETZ("Gesch&uuml;tz","Gesch&uuml;tze"),				// 10
+	/**
+	 * Die Schiffsklasse "Forschungskreuzer"
+	 */
 	FORSCHUNGSKREUZER("Forschungskreuzer"),		// 11
+	/**
+	 * Die Schiffsklasse "Container"
+	 */
 	CONTAINER("Container"),						// 12
+	/**
+	 * Die Schiffsklasse "AWACs"
+	 */
 	AWACS("AWAC", "AWACs"),						// 13
+	/**
+	 * Die Schiffsklasse "Schrotthaufen"
+	 */
 	SCHROTT("Schrott", "Schrotthaufen"),		// 14
+	/**
+	 * Die Schiffsklasse "Traeger"
+	 */
 	TRAEGER("Tr&auml;ger"),						// 15
+	/**
+	 * Die Schiffsklasse "Kommandoschiffe"
+	 */
 	KOMMANDOSCHIFF("Kommandoschiff", "Kommandoschiffe"),		// 16
+	/**
+	 * Die Schiffsklasse "Bomber"
+	 */
 	BOMBER("Bomber"),							// 17
+	/**
+	 * Die Schiffsklasse "Rettungskapseln"
+	 */
 	RETTUNGSKAPSEL("Rettungskapsel", "Rettungskapseln"),		// 18
+	/**
+	 * Die Schiffsklasse "" (nichts)
+	 */
 	EMTPY(""),									// 19
+	/**
+	 * Die Schiffsklasse "Felsbrocken"
+	 */
 	FELSBROCKEN("Felsbrocken");					// 20
 	
 	private String singular;
