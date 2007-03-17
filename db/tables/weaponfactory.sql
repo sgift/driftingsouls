bfd74c6d7a2ab10e8b2675b3c19c4bb0daebbdc4
Christopher Jung <bktheg@web.de>
2007-03-17 15:49:16 +0100

    FK-Constraint fuer weaponfactory.col eingebaut
    committer: Christopher Jung <bktheg@web.de>

--- db/tables/weaponfactory.sql
+++ db/tables/weaponfactory.sql
@@ -1,8 +1,10 @@
 CREATE TABLE `weaponfactory` (
   `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `col` int(10) unsigned NOT NULL default '0',
+  `col` int(11) NOT NULL default '0',
   `count` tinyint(3) unsigned NOT NULL default '0',
   `produces` text NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `col` (`col`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Die Waffenfabriken'; 
+
+ALTER TABLE weaponfactory ADD CONSTRAINT weaponfactory_fk_bases FOREIGN KEY (col) REFERENCES bases(id);
