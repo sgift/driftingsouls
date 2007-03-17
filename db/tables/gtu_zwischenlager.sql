cfbc862a722016b0c85e80a241f6ebf320ce2feb
Christopher Jung <bktheg@web.de>
2007-03-17 14:25:51 +0100

    FK-Contraints fuer gtu_zwischenlager.* eingebaut
    committer: Christopher Jung <bktheg@web.de>

--- db/tables/gtu_zwischenlager.sql
+++ db/tables/gtu_zwischenlager.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `gtu_zwischenlager` (
   `id` int(10) unsigned NOT NULL auto_increment,
-  `posten` int(10) unsigned NOT NULL default '0',
+  `posten` int(11) NOT NULL default '0',
   `user1` int(11) NOT NULL default '0',
   `user2` int(11) NOT NULL default '0',
   `cargo1` text NOT NULL,
@@ -10,3 +10,7 @@ CREATE TABLE `gtu_zwischenlager` (
   PRIMARY KEY  (`id`),
   KEY `posten` (`posten`,`user1`,`user2`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+ALTER TABLE gtu_zwischenlager ADD CONSTRAINT gtu_zwischenlager_fk_ships FOREIGN KEY (posten) REFERENCES ships(id);
+ALTER TABLE gtu_zwischenlager ADD CONSTRAINT gtu_zwischenlager_fk_users1 FOREIGN KEY (user1) REFERENCES users(id);
+ALTER TABLE gtu_zwischenlager ADD CONSTRAINT gtu_zwischenlager_fk_users2 FOREIGN KEY (user2) REFERENCES users(id);
