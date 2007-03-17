d72d49074b2841cc9e31d24b606eb0a4ecf9e973
Christopher Jung <bktheg@web.de>
2007-03-17 14:19:46 +0100

    FK-Constraint fz.col eingebaut
    committer: Christopher Jung <bktheg@web.de>

--- db/tables/fz.sql
+++ db/tables/fz.sql
@@ -6,4 +6,6 @@ CREATE TABLE `fz` (
   `dauer` int(11) NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `col` (`col`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE fz ADD CONSTRAINT fz_fk_bases FOREIGN KEY (col) REFERENCES bases(id);
