b1a0274bc2939ef4e39f209e24e4b5d99b159479
Christopher Jung <bktheg@web.de>
2007-03-17 15:41:41 +0100

    FK-Constraints fuer user_moneytransfer eingebaut
    committer: Christopher Jung <bktheg@web.de>

--- db/tables/user_moneytransfer.sql
+++ db/tables/user_moneytransfer.sql
@@ -10,3 +10,6 @@ CREATE TABLE `user_moneytransfer` (
   PRIMARY KEY  (`id`),
   KEY `from` (`from`,`to`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='RE-Ueberweisungen'; 
+
+ALTER TABLE user_moneytransfer ADD CONSTRAINT user_moneytransfer_fk_users1 FOREIGN KEY (`from`) REFERENCES users(id);
+ALTER TABLE user_moneytransfer ADD CONSTRAINT user_moneytransfer_fk_users2 FOREIGN KEY (`to`) REFERENCES users(id);
