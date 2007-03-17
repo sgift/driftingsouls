c8d2551f150c6c93596dfc54120a865260a1e962
Christopher Jung <bktheg@web.de>
2007-03-17 15:18:17 +0100

    FK-Constraints fuer skn_visits eingebaut
    committer: Christopher Jung <bktheg@web.de>

--- db/tables/skn_visits.sql
+++ db/tables/skn_visits.sql
@@ -6,3 +6,6 @@ CREATE TABLE `skn_visits` (
   PRIMARY KEY  (`id`),
   KEY `user` (`user`, `channel`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Com-Net visits'; 
+
+ALTER TABLE skn_visits ADD CONSTRAINT skn_visits_fk_users FOREIGN KEY (user) REFERENCES users(id);
+ALTER TABLE skn_visits ADD CONSTRAINT skn_visits_fk_skn_channels FOREIGN KEY (channel) REFERENCES skn_channels(id);
