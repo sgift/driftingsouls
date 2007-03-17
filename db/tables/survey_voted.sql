315471cec97aed2ad6fb56eb0f7023aefcaf3875
Christopher Jung <bktheg@web.de>
2007-03-17 15:32:52 +0100

    FK-Constraints fuer survey_voted eingebaut
    committer: Christopher Jung <bktheg@web.de>

--- db/tables/survey_voted.sql
+++ db/tables/survey_voted.sql
@@ -3,3 +3,6 @@ CREATE TABLE `survey_voted` (
   `user_id` int(11) NOT NULL default '0',
   KEY `survey_id` (`survey_id`,`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+ALTER TABLE survey_voted ADD CONSTRAINT survey_voted_fk_surveys FOREIGN KEY (survey_id) REFERENCES surveys(id);
+ALTER TABLE survey_voted ADD CONSTRAINT survey_voted_fk_users FOREIGN KEY (user_id) REFERENCES users(id);
--- src/net/driftingsouls/ds2/server/modules/admin/PlayerDelete.java
+++ src/net/driftingsouls/ds2/server/modules/admin/PlayerDelete.java
@@ -257,6 +257,9 @@ public class PlayerDelete implements AdminPlugin, Loggable {
 		echo.append("L&ouml;sche Statistik 'User-Cargo'...<br />\n");
 		db.update("DELETE FROM stats_user_cargo WHERE user_id="+userid);
 		
+		echo.append("L&ouml;sche Umfrageeintraege...<br />\n");
+		db.update("DELETE FROM survey_voted WHERE user_id="+userid);
+		
 		echo.append("L&ouml;sche Usereintrag...<br />\n");
 		db.update("DELETE FROM users WHERE id="+userid);
 
