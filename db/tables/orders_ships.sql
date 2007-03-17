50ce15255615cb8c217c332167be531a720c6f9c
Christopher Jung <bktheg@web.de>
2007-03-17 14:48:41 +0100

    FK-Constraint orders_ships.type eingebaut
    committer: Christopher Jung <bktheg@web.de>

--- db/tables/orders_ships.sql
+++ db/tables/orders_ships.sql
@@ -4,6 +4,8 @@ CREATE TABLE `orders_ships` (
   PRIMARY KEY  (`type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
 
+ALTER TABLE orders_ships ADD CONSTRAINT orders_ships_fk_ship_types FOREIGN KEY (type) REFERENCES ship_types(id);
+
 INSERT INTO `orders_ships` (`type`, `cost`) VALUES (1, 8);
 INSERT INTO `orders_ships` (`type`, `cost`) VALUES (2, 1);
 INSERT INTO `orders_ships` (`type`, `cost`) VALUES (3, 12);
