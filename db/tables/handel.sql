b6459de9bed30a17030c09747f6f6c2aeb82df35
Christopher Jung <bktheg@web.de>
2007-03-17 14:26:51 +0100

    FK-Contraint fuer handel.who eingebaut
    committer: Christopher Jung <bktheg@web.de>

--- db/tables/handel.sql
+++ db/tables/handel.sql
@@ -7,3 +7,5 @@ CREATE TABLE `handel` (
   `comm` text NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+
+ALTER TABLE handel ADD CONSTRAINT handel_fk_users FOREIGN KEY (who) REFERENCES users(id);
