f6ca4284caaecf5b603f3bd7bcf9ef5135717961
Christopher Jung <bktheg@web.de>
2007-04-01 13:47:07 +0200

    Akademie auf das Templatesystem umgestellt
    committer: Christopher Jung <bktheg@web.de>

--- src/net/driftingsouls/ds2/server/bases/Academy.java
+++ src/net/driftingsouls/ds2/server/bases/Academy.java
@@ -187,21 +187,26 @@ class Academy extends DefaultBuilding {
 		Database db = context.getDatabase();
 		User user = context.getActiveUser();
 		
-		String sess = context.getSession();
-		
 		int newo = context.getRequest().getParameterInt("newo");
 		int train = context.getRequest().getParameterInt("train");
 		int off = context.getRequest().getParameterInt("off");
 		String conf = context.getRequest().getParameterString("conf");
-		StringBuilder echo = new StringBuilder(5000);
 		
-		echo.append("<div class=\"smallfont\">\n");
-		
+		if( !t.set_file( "_BUILDING", "buildings.academy.html" ) ) {
+			context.addError("Konnte das Template-Engine nicht initialisieren");
+			return "";
+		}
+
 		SQLResultRow academy = db.first("SELECT id,train,remain,`upgrade` FROM academy WHERE col="+base.getID());
 		if( academy.isEmpty() ) {
-			echo.append("<span style=\"color:#ff0000; font-weight:bold\">Fehler: Diese Akademie verf&uuml;gt &uuml;ber keinen Akademie-Eintrag in der Datenbank</span><br />\n");
-			return echo.toString();
+			context.addError("Diese Akademie verf&uuml;gt &uuml;ber keinen Akademie-Eintrag in der Datenbank");
+			return "";
 		}
+		
+		t.set_var(	
+				"base.name",	base.getName(),
+				"base.id",		base.getID(),
+				"base.field",	field);
 		
 		//---------------------------------
 		// Einen neue Offiziere ausbilden
@@ -209,21 +214,23 @@ class Academy extends DefaultBuilding {
 		
 		if( newo != 0 ) {
 			if( (academy.getInt("train") == 0) && (academy.getString("upgrade").length() == 0)) {
+				t.set_var("academy.show.trainnewoffi", 1);
+				
 				Cargo cargo = base.getCargo();
 			
 				boolean ok = true;
 				if( cargo.getResourceCount( Resources.SILIZIUM ) < 25 ) {
-					echo.append("<span style=\"color:#ff0000\">Nicht genug Silizium</span><br />\n");
+					t.set_var("trainnewoffi.error", "Nicht genug Silizium");
 					ok = false;
 				}
 				Cargo usercargo = new Cargo( Cargo.Type.STRING, user.getCargo() );
 				if( cargo.getResourceCount( Resources.NAHRUNG )+usercargo.getResourceCount( Resources.NAHRUNG ) < 35 ) {
-					echo.append("<span style=\"color:#ff0000\">Nicht genug Nahrung</span><br />\n");
+					t.set_var("trainnewoffi.error", "Nicht genug Nahrung");
 					ok = false;
 				}
 		
 				if( ok ) {
-					echo.append("Training beginnt<br /><br /><br />\n");
+					t.set_var("trainnewoffi.train", 1);
 		
 					cargo.substractResource( Resources.SILIZIUM, 25 );
 					usercargo.substractResource( Resources.NAHRUNG, 35 );
@@ -247,9 +254,6 @@ class Academy extends DefaultBuilding {
 					}
 		
 				} 
-				else {
-					echo.append("<br /><br />");
-				}
 			}
 		}
 	
@@ -258,12 +262,9 @@ class Academy extends DefaultBuilding {
 		//--------------------------------------
 		
 		if( (train != 0) && (off != 0) ) {
-			if( (academy.getInt("train") == 0) && (academy.getString("upgrade").length() == 0) ) {
+			if( (academy.getInt("train") == 0) && (academy.getString("upgrade").length() == 0) ) {				
 				SQLResultRow offizier = db.first("SELECT * FROM offiziere WHERE id='",off,"'");
-				if( offizier.getString("dest").equals("b "+base.getID()) ) {
-					echo.append(Common.tableBegin( 500, "left" ));
-					
-					echo.append("Trainiere "+offizier.getString("name")+":<br />\n");
+				if( offizier.getString("dest").equals("b "+base.getID()) ) {					
 					Map<Integer,String> dTrain = new HashMap<Integer,String>();
 					dTrain.put(1, "ing");
 					dTrain.put(2, "waf");
@@ -275,46 +276,54 @@ class Academy extends DefaultBuilding {
 					int nk = (int)(offizier.getInt(dTrain.get(train))*1.5d)+1;
 					int dauer = (int)(offizier.getInt(dTrain.get(train))/4d)+1;
 					
+					t.set_var(
+							"academy.show.trainoffi", 1,
+							"trainoffi.id",			off,
+							"trainoffi.trainid",	train,
+							"offizier.name",		Common._plaintext(offizier.getString("name")),
+							"offizier.train.dauer",		dauer,
+							"offizier.train.nahrung", 	nk,
+							"offizier.train.silizium",	sk,
+							"resource.nahrung.image",	Cargo.getResourceImage(Resources.NAHRUNG),
+							"resource.silizium.image",	Cargo.getResourceImage(Resources.SILIZIUM));
+					
 					if( train == 1 ) {
-						echo.append("verbessere Technik");
+						t.set_var("offizier.train.ability", "Technik");
 					} 
 					else if( train == 2 ) {
-						echo.append("verbessere Waffen");
+						t.set_var("offizier.train.ability", "Waffen");
 					} 
 					else if( train == 3 ) {
-						echo.append("verbessere Navigation");
+						t.set_var("offizier.train.ability", "Navigation");
 					}
 					else if( train == 4 ) {
-						echo.append("verbessere Sicherheit");
+						t.set_var("offizier.train.ability", "Sicherheit");
 					} 
 					else if( train == 5 ) {
-						echo.append("verbessere Kommandoeffizienz");
+						t.set_var("offizier.train.ability", "Kommandoeffizienz");
 					}
-					echo.append(" - Kosten: <img src=\""+Configuration.getSetting("URL")+"data/interface/time.gif\" alt=\"Dauer\" />"+dauer+" <img src=\""+Cargo.getResourceImage(Resources.SILIZIUM)+"\" alt=\"\" />"+sk+" <img src=\""+Cargo.getResourceImage(Resources.NAHRUNG)+"\" alt=\"\" />"+nk+"<br />\n");
 					
 					Cargo cargo = base.getCargo();
 
 					boolean ok = true;
 					if( cargo.getResourceCount( Resources.SILIZIUM ) < sk) {
-						echo.append("<span style=\"color:#ff0000\">Nicht genug Silizium</span><br />\n"); 
+						t.set_var("trainoffi.error", "Nicht genug Silizium"); 
 						ok = false;
 					}
 					Cargo usercargo = new Cargo( Cargo.Type.STRING, user.getCargo() );
 					if( cargo.getResourceCount( Resources.NAHRUNG )+usercargo.getResourceCount( Resources.NAHRUNG ) < nk ) {
-						echo.append("<span style=\"color:#ff0000\">Nicht genug Nahrung</span><br />\n"); 
+						t.set_var("trainoffi.error", "Nicht genug Nahrung"); 
 						ok = false;
 					}
 		
 					if( !conf.equals("ok") ) {
-						echo.append("<br /><div style=\"text-align:center\"><a class=\"back\" href=\"./main.php?module=building&amp;sess="+sess+"&amp;col="+base.getID()+"&amp;field="+field+"&amp;train="+train+"&amp;off="+off+"&amp;conf=ok\">Training durchf&uuml;hren</a></div>\n");
-						echo.append(Common.tableEnd());
-						echo.append("<br />\n");
-						
-						return echo.toString();
+						t.set_var("trainoffi.conf",	1);
+						t.parse( "OUT", "_BUILDING" );	
+						return t.getVar("OUT");
 					}
 		
 					if( ok ) {
-						echo.append("<br />Training beginnt<br />\n");
+						t.set_var("trainoffi.train", 1);
 		
 						cargo.substractResource( Resources.SILIZIUM, sk );
 						usercargo.substractResource( Resources.NAHRUNG, nk );
@@ -337,14 +346,10 @@ class Academy extends DefaultBuilding {
 							academy.put("upgrade", off+" "+train);
 							academy.put("remain", dauer);
 						}					
-						echo.append(Common.tableEnd());
-						echo.append("<br />\n");
-						
-						return echo.toString();
+
+						t.parse( "OUT", "_BUILDING" );	
+						return t.getVar("OUT");
 					}
-					
-					echo.append(Common.tableEnd());
-					echo.append("<br />\n");
 				}
 			}
 		}
@@ -355,25 +360,24 @@ class Academy extends DefaultBuilding {
 		//-----------------------------------------------
 		
 		if( (academy.getInt("train") != 0) || (academy.getString("upgrade").length() != 0) ) {
-			echo.append(Common.tableBegin(400, "left"));
-			echo.append("<span style=\"font-weight:bold\">Bildet aus:</span>\n");
-		
+			t.set_var(	
+					"academy.show.training", 1,
+					"training.remain",	academy.getInt("remain"));
+			
+					
 			if( academy.getInt("train") != 0 ) {
-				echo.append(Common._plaintitle(Offiziere.LIST.get(academy.getInt("train")).getString("name"))+"<br />");
+				t.set_var("offizier.name", Common._plaintitle(Offiziere.LIST.get(academy.getInt("train")).getString("name")));
 			}
 			else {
 				String[] upgradeData = StringUtils.split(academy.getString("upgrade"), ' ' );
 				Offizier offizier = Offizier.getOffizierByID(Integer.parseInt(upgradeData[0]));
-				echo.append("<img style=\"vertical-align:middle\" src=\""+offizier.getPicture()+"\" alt=\"\" />\n");
-				echo.append("<a class=\"academy\" href=\"./main.php?module=choff&amp;sess="+sess+"&amp;off="+offizier.getID()+"\">"+Common._plaintitle(offizier.getName())+"</a><br />");
+				
+				t.set_var(
+						"offizier.picture",	offizier.getPicture(),
+						"offizier.id",		offizier.getID(),
+						"offizier.name",	Common._plaintitle(offizier.getName()));
 			}
-		
-			echo.append("<span style=\"font-weight:bold\">Dauer:</span> noch <img align=\"middle\" src=\""+Configuration.getSetting("URL")+"data/interface/time.gif\" alt=\"\" />"+academy.getInt("remain")+" Runden<br />\n");
-		
-			echo.append("</span>");
-			echo.append(Common.tableEnd());
-			echo.append("<br />\n");
-			
+
 			allowActions = false;
 		}
 		
@@ -381,116 +385,57 @@ class Academy extends DefaultBuilding {
 		// Liste: Neue Offiziere ausbilden
 		//---------------------------------
 		if( allowActions ) {
-			echo.append(Common.tableBegin(550, "left"));
-		
-			echo.append("<span style=\"font-weight:bold\">Neuen Offizier ausbilden:</span><br />\n");
-			echo.append("Kosten: <img src=\""+Configuration.getSetting("URL")+"data/interface/time.gif\" alt=\"\" />8 <img src=\""+Cargo.getResourceImage(Resources.SILIZIUM)+"\" alt=\"\" />25 <img src=\""+Cargo.getResourceImage(Resources.NAHRUNG)+"\" alt=\"\" />35<br /><br />");
-			echo.append("<table class=\"noBorderX\" border=\"1\">\n");
-			echo.append("<tr>");
-			echo.append("<td class=\"noBorderX\">Name</td><td class=\"noBorderX\">Technik</td><td class=\"noBorderX\">Waffen</td><td class=\"noBorderX\">Navigation</td><td class=\"noBorderX\">Sicherheit</td><td class=\"noBorderX\">Kommandoeffizienz</td></tr>\n");
+			t.set_var(
+					"academy.show.trainnew",	1,
+					"resource.silizium.image",	Cargo.getResourceImage(Resources.SILIZIUM),
+					"resource.nahrung.image",	Cargo.getResourceImage(Resources.NAHRUNG));
+			
+			t.set_block("_BUILDING", "academy.trainnew.listitem", "academy.trainnew.list");
+			
 			for( SQLResultRow offi : Offiziere.LIST.values() ) {
-				echo.append("<tr>\n");
-				echo.append("<td class=\"noBorderX\"><a class=\"academy\" href=\"./main.php?module=building&amp;sess="+sess+"&amp;col="+base.getID()+"&amp;field="+field+"&amp;newo="+offi.getInt("id")+"\">"+Common._title(offi.getString("name"))+"</a></td>\n");
-				echo.append("<td class=\"noBorderX\" align=\"center\">"+offi.getInt("ing")+"</td>\n");
-				echo.append("<td class=\"noBorderX\" align=\"center\">"+offi.getInt("waf")+"</td>\n");
-				echo.append("<td class=\"noBorderX\" align=\"center\">"+offi.getInt("nav")+"</td>\n");
-				echo.append("<td class=\"noBorderX\" align=\"center\">"+offi.getInt("sec")+"</td>\n");
-				echo.append("<td class=\"noBorderX\" align=\"center\">"+offi.getInt("com")+"</td>\n");
-				echo.append("</tr>\n");
+				t.set_var( 
+						"offizier.id",		offi.getInt("id"),
+						"offizier.name",	Common._title(offi.getString("name")),
+						"offizier.ing",		offi.getInt("ing"),
+						"offizier.waf",		offi.getInt("waf"),
+						"offizier.nav",		offi.getInt("nav"),
+						"offizier.sec",		offi.getInt("sec"),
+						"offizier.com",		offi.getInt("com"));
+				
+				t.parse("academy.trainnew.list", "academy.trainnew.listitem", true);
 			}
-			echo.append("</table><br />\n");
-			echo.append(Common.tableEnd());
-			echo.append("<br />\n");
 		}
 		
 		//---------------------------------
 		// Liste: "Upgrade" von Offizieren
 		//---------------------------------
 		
-		boolean firstEntry = true;
+		t.set_var(
+				"academy.show.offilist", 1,
+				"offilist.allowactions", allowActions);
+		
+		t.set_block("_BUILDING", "academy.offilist.listitem", "academy.offilist.list");
 		
 		SQLQuery offizier = db.query("SELECT * FROM offiziere WHERE dest='b "+base.getID()+"'");
-		while( offizier.next() ) {
-			if( firstEntry ) {
-				echo.append(Common.tableBegin(550, "left"));
-				echo.append("<span style=\"font-weight:bold\">Anwesende Offiziere:</span><br /><br />\n");
-				echo.append("<table class=\"noBorderX\" border=\"1\">\n");
-				echo.append("<tr>\n");
-				echo.append("<td class=\"noBorderX\">Name</td>\n");
-				echo.append("<td class=\"noBorderX\">Technik</td>\n");
-				echo.append("<td class=\"noBorderX\">Waffen</td>\n");
-				echo.append("<td class=\"noBorderX\">Navigation</td>\n");
-				echo.append("<td class=\"noBorderX\">Sicherheit</td>\n");
-				echo.append("<td class=\"noBorderX\">Kommandoeffizienz</td>\n");
-				echo.append("<td class=\"noBorderX\">Spezial</td>\n");
-				echo.append("</tr>\n");
-
-				firstEntry = false;
-			}
+		while( offizier.next() ) {			
 			Offizier offi = new Offizier( offizier.getRow() );
-			echo.append("<tr>\n");
-			echo.append("<td class=\"noBorderX\" style=\"white-space:nowrap\">\n");
-			echo.append("<img style=\"vertical-align:middle\" src=\""+offi.getPicture()+"\" alt=\"\" />\n");
-			echo.append("<a class=\"academy\" href=\"./main.php?module=choff&amp;sess="+sess+"&amp;off="+offi.getID()+"\">"+Common._plaintitle(offi.getName())+"</a>\n");
-			echo.append("</td>");
 			
-			echo.append("<td class=\"noBorderX\" align=\"center\">\n");
-			if( allowActions ) {
-				echo.append("<a class=\"academy\" href=\"./main.php?module=building&amp;sess="+sess+"&amp;col="+base.getID()+"&amp;field="+field+"&amp;train=1&amp;off="+offi.getID()+"\">"+offi.getAbility(Offizier.Ability.ING)+"</a>\n");
-			}
-			else {
-				echo.append(offi.getAbility(Offizier.Ability.ING));
-			}
-			echo.append("</td>");
+			t.set_var(
+					"offizier.picture",	offi.getPicture(),
+					"offizier.id",		offi.getID(),
+					"offizier.name",	Common._plaintitle(offi.getName()),
+					"offizier.ing",		offi.getAbility(Offizier.Ability.ING),
+					"offizier.waf",		offi.getAbility(Offizier.Ability.WAF),
+					"offizier.nav",		offi.getAbility(Offizier.Ability.NAV),
+					"offizier.sec",		offi.getAbility(Offizier.Ability.SEC),
+					"offizier.com",		offi.getAbility(Offizier.Ability.COM),
+					"offizier.special",	offi.getSpecial().getName() );
 			
-			echo.append("<td class=\"noBorderX\" align=\"center\">\n");
-			if( allowActions ) {
-				echo.append("<a class=\"academy\" href=\"./main.php?module=building&amp;sess="+sess+"&amp;col="+base.getID()+"&amp;field="+field+"&amp;train=2&amp;off="+offi.getID()+"\">"+offi.getAbility(Offizier.Ability.WAF)+"</a>\n");
-			}
-			else {
-				echo.append(offi.getAbility(Offizier.Ability.WAF));
-			}
-			echo.append("</td>");
-			
-			echo.append("<td class=\"noBorderX\" align=\"center\">\n");
-			if( allowActions ) {
-				echo.append("<a class=\"academy\" href=\"./main.php?module=building&amp;sess="+sess+"&amp;col="+base.getID()+"&amp;field="+field+"&amp;train=3&amp;off="+offi.getID()+"\">"+offi.getAbility(Offizier.Ability.NAV)+"</a>\n");
-			}
-			else {
-				echo.append(offi.getAbility(Offizier.Ability.NAV));
-			}
-			echo.append("</td>");
-			
-			echo.append("<td class=\"noBorderX\" align=\"center\">\n");
-			if( allowActions ) {
-				echo.append("<a class=\"academy\" href=\"./main.php?module=building&amp;sess="+sess+"&amp;col="+base.getID()+"&amp;field="+field+"&amp;train=4&amp;off="+offi.getID()+"\">"+offi.getAbility(Offizier.Ability.SEC)+"</a>\n");
-			}
-			else {
-				echo.append(offi.getAbility(Offizier.Ability.SEC));
-			}
-			echo.append("</td>");
-			
-			echo.append("<td class=\"noBorderX\" align=\"center\">\n");
-			if( allowActions ) {
-				echo.append("<a class=\"academy\" href=\"./main.php?module=building&amp;sess="+sess+"&amp;col="+base.getID()+"&amp;field="+field+"&amp;train=5&amp;off="+offi.getID()+"\">"+offi.getAbility(Offizier.Ability.COM)+"</a>\n");
-			}
-			else {
-				echo.append(offi.getAbility(Offizier.Ability.COM));
-			}
-			echo.append("</td>");
-			
-			echo.append("<td class=\"noBorderX\">"+offi.getSpecial().getName()+"</td>\n");
-			echo.append("</tr>\n");
-		}
-		if( !firstEntry ) {
-			echo.append("</table><br />\n");
-			echo.append(Common.tableEnd());
-			echo.append("<br />\n");
+			t.parse("academy.offilist.list", "academy.offilist.listitem", true);
 		}
 		offizier.free();
 		
-		echo.append("<br /></div>");
-		
-		return echo.toString();
+		t.parse( "OUT", "_BUILDING" );	
+		return t.getVar("OUT");
 	}
 }
