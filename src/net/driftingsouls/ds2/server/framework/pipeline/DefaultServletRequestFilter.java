6779a1216f8c8dadf49fd72350e424d20ee8c0db
Christopher Jung <bktheg@web.de>
2008-04-18 09:34:48 +0200

    Bessere Fehlerbehandlung am Anfang und Ende einer Request
    committer: Christopher Jung <bktheg@web.de>

--- src/net/driftingsouls/ds2/server/framework/pipeline/DefaultServletRequestFilter.java
+++ src/net/driftingsouls/ds2/server/framework/pipeline/DefaultServletRequestFilter.java
@@ -21,13 +21,15 @@ package net.driftingsouls.ds2.server.framework.pipeline;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Date;
+import java.util.Iterator;
+import java.util.Map;
 
+import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
-import javax.servlet.Filter;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -57,68 +59,92 @@ public class DefaultServletRequestFilter implements Filter, Loggable {
 			LOG.error(this.getClass().getName()+" konnte Request nicht verarbeiten");
 			return;
 		}
-
+		
 		HttpServletRequest httpRequest = (HttpServletRequest)req;
 		HttpServletResponse httpResponse = (HttpServletResponse)resp;
 		
-		//Refferer verstecken, um Abgreifen der sessid zu vermeiden
-		httpResponse.setHeader("Referer", "http://ds.drifting-souls.net");
-		
-		Request request = new HttpRequest(httpRequest);
-		Response response = new HttpResponse(httpResponse);
-		
-		final BasicContext context = new BasicContext(request, response);
-		
-		context.putVariable(HttpServlet.class, "response", httpResponse);
-		context.putVariable(HttpServlet.class, "request", httpRequest);
-		context.putVariable(HttpServlet.class, "context", this.context);
-		context.putVariable(HttpServlet.class, "chain", chain);
-		
 		try {
+			// Refferer verstecken, um Abgreifen der sessid zu vermeiden
+			httpResponse.setHeader("Referer", "http://ds.drifting-souls.net");
+			
+			Request request = new HttpRequest(httpRequest);
+			Response response = new HttpResponse(httpResponse);
+			
+			BasicContext context = null;
+			
 			try {
-				Pipeline pipeline = PipelineConfig.getPipelineForContext(context);
+				context = new BasicContext(request, response);
+				context.putVariable(HttpServlet.class, "response", httpResponse);
+				context.putVariable(HttpServlet.class, "request", httpRequest);
+				context.putVariable(HttpServlet.class, "context", this.context);
+				context.putVariable(HttpServlet.class, "chain", chain);
 				
-				if( pipeline != null ) {
-					pipeline.execute(context);
-				}
-				else {
-					throw new Exception("Unable to find a suitable rule for URL '"+context.getRequest().getRequestURL()+(context.getRequest().getQueryString() != null ? "?"+context.getRequest().getQueryString() : "")+"'");
-				}
+				context.revalidate();
 				
-				context.getResponse().send();
+				try {
+					Pipeline pipeline = PipelineConfig.getPipelineForContext(context);
+					
+					if( pipeline != null ) {
+						pipeline.execute(context);
+					}
+					else {
+						throw new Exception("Unable to find a suitable rule for URL '"+context.getRequest().getRequestURL()+(context.getRequest().getQueryString() != null ? "?"+context.getRequest().getQueryString() : "")+"'");
+					}
+					
+					context.getResponse().send();
+				}
+				catch( Throwable e ) {
+					context.rollback();
+					
+					mailThrowable(httpRequest, context, e);
+					
+					e.printStackTrace();
+					httpResponse.setContentType("text/html");
+					PrintWriter writer = httpResponse.getWriter();
+					writer.append("<html><head><title>Drifting Souls Server Framework</title></head>");
+					writer.append("<body>");
+					writer.append("<table border=\"0\"><tr><td>\n");
+					writer.append("<div align=\"center\">\n");
+					writer.append("<h1>Drifting Souls Server Framework</h1>");
+					writer.append("Unhandled Exception "+e.getClass().getName()+" during pipeline execution detected<br />\n");
+					writer.append("Reason: "+e.getMessage()+"</div>\n");
+					writer.append("<hr style=\"height:1px; border:0px; background-color:#606060; color:#606060\" />");
+					StackTraceElement[] st = e.getStackTrace();
+					for( int i=0; i < st.length; i++ ) {
+						writer.append(st[i].toString()+"<br />\n");
+					}
+					writer.append("</td></tr></table></body></html>");
+				}
 			}
-			catch( Throwable e ) {
-				context.rollback();
-				
-				StringBuilder msg = new StringBuilder(100);
-				msg.append("Time: "+new Date()+"\n");
-				msg.append("URI: "+httpRequest.getRequestURI()+"\n");
-				msg.append("QUERY_STRING: "+httpRequest.getQueryString()+"\n");
-				msg.append("Session: "+httpRequest.getParameter("sess")+"\n");
-				msg.append("User: "+(context != null && context.getActiveUser() != null ? context.getActiveUser().getId() : "none")+"\n");
-				Common.mailThrowable(e, "Framework Exception", msg.toString());
-				
-				e.printStackTrace();
-				httpResponse.setContentType("text/html");
-				PrintWriter writer = httpResponse.getWriter();
-				writer.append("<html><head><title>Drifting Souls Server Framework</title></head>");
-				writer.append("<body>");
-				writer.append("<table border=\"0\"><tr><td>\n");
-				writer.append("<div align=\"center\">\n");
-				writer.append("<h1>Drifting Souls Server Framework</h1>");
-				writer.append("Unhandled Exception "+e.getClass().getName()+" during pipeline execution detected<br />\n");
-				writer.append("Reason: "+e.getMessage()+"</div>\n");
-				writer.append("<hr style=\"height:1px; border:0px; background-color:#606060; color:#606060\" />");
-				StackTraceElement[] st = e.getStackTrace();
-				for( int i=0; i < st.length; i++ ) {
-					writer.append(st[i].toString()+"<br />\n");
+			finally {
+				if( context != null ) {
+					context.free();
 				}
-				writer.append("</td></tr></table></body></html>");
 			}
 		}
-		finally {
-			context.free();
+		catch( Throwable e ) {
+			mailThrowable(httpRequest, null, e);
 		}
+	}
+
+	private void mailThrowable(HttpServletRequest httpRequest, BasicContext context, Throwable e) {
+		StringBuilder msg = new StringBuilder(100);
+		msg.append("Time: "+new Date()+"\n");
+		msg.append("URI: "+httpRequest.getRequestURI()+"\n");
+		msg.append("PARAMS:\n");
+		for( Iterator iter=httpRequest.getParameterMap().entrySet().iterator(); iter.hasNext(); ) {
+			Map.Entry entry = (Map.Entry)iter.next();
+			msg.append("\t* "+entry.getKey()+" = "+entry.getValue()+"\n");
+		}
+		
+		msg.append("QUERY_STRING: "+httpRequest.getQueryString()+"\n");
+		msg.append("Session: "+httpRequest.getParameter("sess")+"\n");
+		
+		if( context != null ) {
+			msg.append("User: "+(context.getActiveUser() != null ? context.getActiveUser().getId() : "none")+"\n");
+		}
+		
+		Common.mailThrowable(e, (context == null ? "Fatal " : "")+"Framework Exception", msg.toString());
 	}
 
 	public void init(FilterConfig filterConfig) throws ServletException {
