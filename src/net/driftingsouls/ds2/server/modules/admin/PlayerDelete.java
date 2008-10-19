d43b6b41207cd6524a8c7c207799a3d638ce48ba
Simon Dietsch <rhonabwy@sternenbund.org>
2008-10-19 11:04:06 +0200

    [bug] Spieler mit GTU Zwischenlager sollten jetzt geloescht werden koennen.

--- src/net/driftingsouls/ds2/server/modules/admin/PlayerDelete.java
+++ src/net/driftingsouls/ds2/server/modules/admin/PlayerDelete.java
@@ -39,7 +39,6 @@ import net.driftingsouls.ds2.server.framework.Common;
 import net.driftingsouls.ds2.server.framework.Configuration;
 import net.driftingsouls.ds2.server.framework.Context;
 import net.driftingsouls.ds2.server.framework.ContextMap;
-import net.driftingsouls.ds2.server.framework.Loggable;
 import net.driftingsouls.ds2.server.framework.db.Database;
 import net.driftingsouls.ds2.server.modules.AdminController;
 import net.driftingsouls.ds2.server.scripting.ScriptParserContext;
@@ -47,25 +46,33 @@ import net.driftingsouls.ds2.server.scripting.entities.RunningQuest;
 import net.driftingsouls.ds2.server.ships.Ship;
 import net.driftingsouls.ds2.server.tasks.Taskmanager;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 /**
  * Ermoeglicht das Einloggen in einen anderen Account ohne Passwort
+ * 
  * @author Christopher Jung
- *
+ * 
  */
-@AdminMenuEntry(category="Spieler", name="Spieler l&ouml;schen")
-public class PlayerDelete implements AdminPlugin, Loggable {
+@AdminMenuEntry(category = "Spieler", name = "Spieler l&ouml;schen")
+public class PlayerDelete implements AdminPlugin
+{
+	private static final Log log = LogFactory.getLog(PlayerDelete.class);
 
-	public void output(AdminController controller, String page, int action) {
+	public void output(AdminController controller, String page, int action)
+	{
 		Context context = ContextMap.getContext();
 		StringBuffer echo = context.getResponse().getContent();
 		Database database = context.getDatabase();
 		org.hibernate.Session db = context.getDB();
-		
+
 		int userid = context.getRequest().getParameterInt("userid");
-		
+
 		String sess = context.getSession();
-		
-		if( userid == 0 ) {
+
+		if( userid == 0 )
+		{
 			echo.append(Common.tableBegin(400, "center"));
 			echo.append("Hinweis: Es gibt KEINE Sicherheitsabfrage!<br />\n");
 			echo.append("<form action=\"./ds\" method=\"post\">");
@@ -73,86 +80,102 @@ public class PlayerDelete implements AdminPlugin, Loggable {
 			echo.append("<tr><td class=\"noBorderX\" width=\"60\">Userid:</td><td class=\"noBorderX\">");
 			echo.append("<input type=\"text\" name=\"userid\" size=\"6\" />");
 			echo.append("</td></tr>\n");
-			echo.append("<tr><td class=\"noBorderX\" colspan=\"2\" align=\"center\"><input type=\"hidden\" name=\"sess\" value=\""+sess+"\" />");
-			echo.append("<input type=\"hidden\" name=\"page\" value=\""+page+"\" />");
-			echo.append("<input type=\"hidden\" name=\"act\" value=\""+action+"\" />");
+			echo.append("<tr><td class=\"noBorderX\" colspan=\"2\" align=\"center\"><input type=\"hidden\" name=\"sess\" value=\"" + sess + "\" />");
+			echo.append("<input type=\"hidden\" name=\"page\" value=\"" + page + "\" />");
+			echo.append("<input type=\"hidden\" name=\"act\" value=\"" + action + "\" />");
 			echo.append("<input type=\"hidden\" name=\"module\" value=\"admin\" />\n");
 			echo.append("<input type=\"submit\" value=\"l&ouml;schen\" style=\"width:100px\"/></td></tr>");
 			echo.append("</table>\n");
 			echo.append("</form>");
 			echo.append(Common.tableEnd());
-			
+
 			return;
 		}
-
-		echo.append(Common.tableBegin(500,"left"));
-		User user = (User)db.get(User.class, userid);
 		
-		if( (user.getAlly() != null) && (user.getAlly().getPresident() == user) ) {
-			echo.append("Der Spieler "+userid+" ist Pr&auml;sident einer Allianz.<br />\n");
+		log.info("Loesche Spieler "+userid);
+
+		echo.append(Common.tableBegin(500, "left"));
+		User user = (User)db.get(User.class, userid);
+
+		if( (user.getAlly() != null) && (user.getAlly().getPresident() == user) )
+		{
+			echo.append("Der Spieler " + userid + " ist Pr&auml;sident einer Allianz.<br />\n");
 			echo.append("Die Allianz muss zuerst gel&ouml;scht werden oder einen anderen Pr&auml;sidenten bekommen, bevor der Spieler gel&ouml;scht werden kann.<br />\n");
 			echo.append(Common.tableEnd());
-			
+
 			return;
 		}
 
 		ScriptEngine scriptparser = context.get(ContextCommon.class).getScriptParser("DSQuestScript");
-		
+
 		echo.append("Beende Quests....<br />\n");
-		List rquestList = db.createQuery("from RunningQuest where user= :user")
-			.setEntity("user", user)
+		List<?> rquestList = db.createQuery("from RunningQuest where user= :user")
+			.setEntity("user",user)
 			.list();
-		for( Iterator iter=rquestList.iterator(); iter.hasNext(); ) {
+		for( Iterator<?> iter = rquestList.iterator(); iter.hasNext(); )
+		{
 			RunningQuest rquest = (RunningQuest)iter.next();
 
-			try {
-				scriptparser.setContext(
-						ScriptParserContext.fromStream(rquest.getExecData().getBinaryStream())
-				);
-				final Bindings engineBindings = scriptparser.getContext().getBindings(ScriptContext.ENGINE_SCOPE);
-				
+			try
+			{
+				scriptparser.setContext(ScriptParserContext.fromStream(rquest.getExecData()
+						.getBinaryStream()));
+				final Bindings engineBindings = scriptparser.getContext().getBindings(
+						ScriptContext.ENGINE_SCOPE);
+
 				engineBindings.put("USER", userid);
-				engineBindings.put("QUEST", "r"+rquest.getId());
+				engineBindings.put("QUEST", "r" + rquest.getId());
 				engineBindings.put("_PARAMETERS", "0");
 				scriptparser.eval(":0\n!ENDQUEST\n!QUIT");
 			}
-			catch( Exception e ) {
-				echo.append("Fehler beim Beenden des Quests "+rquest.getQuest().getId()+": "+e);
-				LOG.error(e,e);
+			catch( Exception e )
+			{
+				echo.append("Fehler beim Beenden des Quests " + rquest.getQuest().getId() + ": "+ e);
+				log.error(e, e);
 				echo.append(Common.tableEnd());
-				
+
 				context.rollback();
 			}
 		}
-		
+
 		echo.append("Loesche 'Abgeschlossen'-Status bei Quests<br />\n");
 		db.createQuery("delete from CompletedQuest where user= :user")
 			.setEntity("user", user)
 			.executeUpdate();
-		
-		if( user.getAlly() != null ) {
+
+		if( user.getAlly() != null )
+		{
 			echo.append("Stelle fest ob die Ally jetzt zu wenig Member hat\n");
-			
+
 			Ally ally = user.getAlly();
-				
+
 			// Allianzen mit einem NPC als Praesidenten koennen auch mit 1 oder 2 Membern existieren
-			if( ally.getPresident().getId() > 0 ) {
-				long count = ally.getMemberCount()-1;
-				if( count < 3 ) {
-					Taskmanager.getInstance().addTask(Taskmanager.Types.ALLY_LOW_MEMBER, 21, Integer.toString(ally.getId()), "", "" );
-	
+			if( ally.getPresident().getId() > 0 )
+			{
+				long count = ally.getMemberCount() - 1;
+				if( count < 3 )
+				{
+					Taskmanager.getInstance().addTask(Taskmanager.Types.ALLY_LOW_MEMBER, 21,
+							Integer.toString(ally.getId()), "", "");
+
 					final User sourceUser = (User)db.get(User.class, 0);
-					
+
 					List<User> supermembers = ally.getSuperMembers();
-					for( User supermember : supermembers ) {
-						if( supermember.getId() == userid ) {
+					for( User supermember : supermembers )
+					{
+						if( supermember.getId() == userid )
+						{
 							continue;
 						}
-						
-						PM.send( sourceUser, supermember.getId(), "Drohende Allianzaufl&oum;sung", "[Automatische Nachricht]\nAchtung!\n" +
-						"Durch das L&ouml;schen eines Allianzmitglieds hat deine Allianz zu wenig Mitglieder " +
-						"um weiterhin zu bestehen. Du hast nun 21 Ticks Zeit diesen Zustand zu &auml;ndern. " +
-						"Andernfalls wird die Allianz aufgel&ouml;&szlig;t.");
+
+						PM.send(
+							sourceUser,
+							supermember.getId(),
+							"Drohende Allianzaufl&oum;sung",
+							"[Automatische Nachricht]\nAchtung!\n"
+									+ "Durch das L&ouml;schen eines Allianzmitglieds hat deine Allianz zu wenig Mitglieder "
+									+ "um weiterhin zu bestehen. Du hast nun 21 Ticks Zeit diesen Zustand zu &auml;ndern. "
+									+ "Andernfalls wird die Allianz aufgel&ouml;&szlig;t.");
 					}
 
 					echo.append("....sie hat jetzt zu wenig");
@@ -160,6 +183,13 @@ public class PlayerDelete implements AdminPlugin, Loggable {
 			}
 			echo.append("<br />\n");
 		}
+		echo.append("Entferne GTU Zwischenlager...<br />\n");
+		db.createQuery("delete from GtuZwischenlager where user1=?")
+			.setInteger(0, userid)
+			.executeUpdate();
+		db.createQuery("delete from GtuZwischenlager where user2=?")
+			.setInteger(0, userid)
+			.executeUpdate();
 		
 		echo.append("Entferne User-Values...<br />\n");
 		db.createQuery("delete from UserValue where user=?")
@@ -170,59 +200,66 @@ public class PlayerDelete implements AdminPlugin, Loggable {
 		db.createQuery("delete from ComNetVisit where user=?")
 			.setInteger(0, userid)
 			.executeUpdate();
-		
+
 		echo.append("Ordne Comnet-Posts ID 0 zu...<br />\n");
 		db.createQuery("update ComNetEntry set user=0 where user=?")
 			.setInteger(0, userid)
 			.executeUpdate();
 
 		echo.append("Entferne user-forschungen...<br />\n");
-		db.createQuery("delete from UserResearch where owner=?").setInteger(0, userid).executeUpdate();
+		db.createQuery("delete from UserResearch where owner=?")
+			.setInteger(0, userid)
+			.executeUpdate();
 
-		//Schiffe
+		// Schiffe
 		echo.append("Entferne Schiffe...\n");
 		int count = 0;
-		
-		List ships = db.createQuery("from Ship where owner=?")
+
+		List<?> ships = db.createQuery("from Ship where owner=?")
 			.setEntity(0, user)
 			.list();
-		for( Iterator iter=ships.iterator(); iter.hasNext(); ) {
+		for( Iterator<?> iter = ships.iterator(); iter.hasNext(); )
+		{
 			Ship aship = (Ship)iter.next();
 			aship.destroy();
 			count++;
 		}
-		
-		echo.append(count+" Schiffe entfernt<br />\n");
 
-		//Basen
+		echo.append(count + " Schiffe entfernt<br />\n");
+
+		// Basen
 		List<Base> baselist = new ArrayList<Base>();
-		
-		List baseList = db.createQuery("from Base where owner=?")
+
+		List<?> baseList = db.createQuery("from Base where owner=?")
 			.setInteger(0, userid)
 			.list();
-		for( Iterator iter=baseList.iterator(); iter.hasNext(); ) {
+		for( Iterator<?> iter = baseList.iterator(); iter.hasNext(); )
+		{
 			Base base = (Base)iter.next();
 			baselist.add(base);
 		}
-		
+
 		echo.append("&Uuml;bereigne Basen an Spieler 0 (+ reset)...\n");
 
 		User nullUser = (User)db.get(User.class, 0);
-		
-		for( Base base : baselist ) {
+
+		for( Base base : baselist )
+		{
 			Integer[] bebauung = base.getBebauung();
-			for( int i=0; i < bebauung.length; i++ ) {
-				if( bebauung[i] == 0 ) {
+			for( int i = 0; i < bebauung.length; i++ )
+			{
+				if( bebauung[i] == 0 )
+				{
 					continue;
 				}
-				
+
 				Building building = Building.getBuilding(bebauung[i]);
 				building.cleanup(context, base);
 			}
-			
+
 			base.setOwner(nullUser);
 			base.setName("Verlassener Asteroid");
-			base.setActive(new Integer[] {0});
+			base.setActive(new Integer[] { 0 });
 			base.setCore(0);
 			base.setCoreActive(false);
 			base.setEnergy(0);
@@ -232,7 +269,7 @@ public class PlayerDelete implements AdminPlugin, Loggable {
 			base.setAutoGTUActs(new ArrayList<AutoGTUAction>());
 		}
 
-		echo.append(baselist.size()+" Basen bearbeitet<br />\n");
+		echo.append(baselist.size() + " Basen bearbeitet<br />\n");
 
 		echo.append("Entferne Handelseintr&auml;ge...<br />\n");
 		db.createQuery("delete from Handel where who=?")
@@ -243,7 +280,7 @@ public class PlayerDelete implements AdminPlugin, Loggable {
 		db.createQuery("update Versteigerung set bieter=-2 where bieter=?")
 			.setInteger(0, userid)
 			.executeUpdate();
-		
+
 		db.createQuery("update Versteigerung set owner=-2 where owner=?")
 			.setInteger(0, userid)
 			.executeUpdate();
@@ -251,23 +288,22 @@ public class PlayerDelete implements AdminPlugin, Loggable {
 		db.createQuery("update PaketVersteigerung set bieter=-2 where bieter=?")
 			.setInteger(0, userid)
 			.executeUpdate();
-		
+
 		echo.append("L&ouml;sche PM's...\n");
 		int affRows = db.createQuery("delete from PM where empfaenger = :user")
 			.setEntity("user", user)
 			.executeUpdate();
-		echo.append(affRows+" gel&ouml;scht<br />\n");
-		
+		echo.append(affRows + " gel&ouml;scht<br />\n");
+
 		db.createQuery("update PM set sender=0 where sender = :user")
 			.setEntity("user", user)
 			.executeUpdate();
-		
+
 		echo.append("L&ouml;sche PM-Ordner...\n");
 		count = db.createQuery("delete from Ordner where owner=?")
 			.setInteger(0, userid)
 			.executeUpdate();
-		echo.append(count+" entfernt<br />");
-		
+		echo.append(count + " entfernt<br />");
 
 		echo.append("L&ouml;sche Diplomatieeintr&auml;ge...<br />\n");
 		db.createQuery("delete from UserRelation where user=? or target=?")
@@ -281,42 +317,43 @@ public class PlayerDelete implements AdminPlugin, Loggable {
 			.executeUpdate();
 
 		echo.append("L&ouml;sche Userlogo...<br />\n");
-		new File(Configuration.getSetting("ABSOLUTE_PATH")+"data/logos/user/"+userid+".gif").delete();
-		
+		new File(Configuration.getSetting("ABSOLUTE_PATH") + "data/logos/user/" + userid + ".gif")
+				.delete();
+
 		echo.append("L&ouml;sche Offiziere...\n");
 		count = db.createQuery("delete from Offizier where owner=?")
 			.setInteger(0, userid)
 			.executeUpdate();
-		echo.append(count+" entfernt<br />");
-		
+		echo.append(count + " entfernt<br />");
+
 		echo.append("L&ouml;sche Shop-Auftraege...<br />\n");
 		db.createQuery("delete from FactionShopOrder where user=?")
 			.setInteger(0, userid)
 			.executeUpdate();
-		
+
 		echo.append("L&ouml;sche Sessions...<br />\n");
 		db.createQuery("delete from Session where id=?")
 			.setInteger(0, userid)
 			.executeUpdate();
-		
+
 		echo.append("L&ouml;sche Statistik 'Item-Locations'...<br />\n");
 		db.createQuery("delete from StatItemLocations where user=?")
 			.setInteger(0, userid)
 			.executeUpdate();
-		
+
 		echo.append("L&ouml;sche Statistik 'User-Cargo'...<br />\n");
 		db.createQuery("delete from StatUserCargo where user=?")
 			.setInteger(0, userid)
 			.executeUpdate();
-		
+
 		echo.append("L&ouml;sche Umfrageeintraege...<br />\n");
-		database.update("DELETE FROM survey_voted WHERE user_id="+userid);
-		
+		database.update("DELETE FROM survey_voted WHERE user_id=" + userid);
+
 		echo.append("L&ouml;sche Usereintrag...<br />\n");
 		db.delete(user);
-		
-		echo.append("<br />Spieler "+userid+" gel&ouml;scht!<br />\n");		
-		
+
+		echo.append("<br />Spieler " + userid + " gel&ouml;scht!<br />\n");
+
 		echo.append(Common.tableEnd());
 	}
 
