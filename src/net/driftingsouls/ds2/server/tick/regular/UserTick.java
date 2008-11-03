ca33b2434279076c9875b14eee6f42fec6aea394
Christopher Jung <bktheg@web.de>
2008-11-03 18:11:14 +0100

    [ref] Der Usertick ist nun unempfindlicher gegen StaleObjectStateExceptions (= laeuft stabiler)

--- src/net/driftingsouls/ds2/server/tick/regular/UserTick.java
+++ src/net/driftingsouls/ds2/server/tick/regular/UserTick.java
@@ -1,5 +1,20 @@
-/**
- * 
+/*
+ *	Drifting Souls 2
+ *	Copyright (c) 2008 Christopher Jung
+ *
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License as published by the Free Software Foundation; either
+ *	version 2.1 of the License, or (at your option) any later version.
+ *
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
+ *
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 package net.driftingsouls.ds2.server.tick.regular;
 
@@ -8,6 +23,7 @@ import java.util.List;
 import net.driftingsouls.ds2.server.cargo.Cargo;
 import net.driftingsouls.ds2.server.cargo.Resources;
 import net.driftingsouls.ds2.server.entities.User;
+import net.driftingsouls.ds2.server.framework.Common;
 import net.driftingsouls.ds2.server.framework.ConfigValue;
 import net.driftingsouls.ds2.server.tick.TickController;
 
@@ -28,10 +44,9 @@ public class UserTick extends TickController
 		this.db = getDB();
 	}
 	
-	@SuppressWarnings("unchecked")
 	private List<User> getActiveUserList()
 	{
-		return db.createQuery("from User where vaccount=0 or wait4vac>0").list();
+		return Common.cast(db.createQuery("from User where vaccount=0 or wait4vac>0").list());
 	}
 
 	@Override
@@ -42,18 +57,34 @@ public class UserTick extends TickController
 		List<User> users = getActiveUserList();
 		for(User user: users)
 		{
-			Cargo usercargo = new Cargo( Cargo.Type.STRING, user.getCargo());
-			
-			//Rot food
-			double rottenFoodPercentage = foodPoolDegeneration + user.getFoodpooldegeneration();
-			long food = usercargo.getResourceCount(Resources.NAHRUNG);
-			long rottenFood = (long)(food*(rottenFoodPercentage/100.0));
-			
-			log(user.getId()+": "+rottenFood);
-			
-			usercargo.setResource(Resources.NAHRUNG, food - rottenFood);
-			
-			user.setCargo(usercargo.save());
+			try {
+				Cargo usercargo = new Cargo( Cargo.Type.STRING, user.getCargo());
+				
+				//Rot food
+				double rottenFoodPercentage = foodPoolDegeneration + user.getFoodpooldegeneration();
+				long food = usercargo.getResourceCount(Resources.NAHRUNG);
+				long rottenFood = (long)(food*(rottenFoodPercentage/100.0));
+				
+				log(user.getId()+": "+rottenFood);
+				
+				usercargo.setResource(Resources.NAHRUNG, food - rottenFood);
+				
+				user.setCargo(usercargo.save());
+				
+				getContext().commit();
+			}
+			catch( Exception e )
+			{
+				getContext().rollback();
+				
+				this.log("User Tick - User #"+user.getId()+" failed: "+e);
+				e.printStackTrace();
+				Common.mailThrowable(e, "UserTick - User #"+user.getId()+" Exception", "");
+			}
+			finally
+			{
+				db.evict(user);
+			}
 		}
 	}
 
