dde7452876a67c8a4e87a6a9c93ddea331a76756
Sebastian Gift <Madison@gt-knm.de>
2008-11-18 18:21:37 +0100

    [bug] Mittels Manipulationen an der URL des KS konnten Schiffe vorzeitig zur Flucht bewegt werden. Diese Luecke ist nun gestopft.

--- src/net/driftingsouls/ds2/server/modules/ks/KSFluchtAllAction.java
+++ src/net/driftingsouls/ds2/server/modules/ks/KSFluchtAllAction.java
@@ -21,10 +21,8 @@ package net.driftingsouls.ds2.server.modules.ks;
 import java.io.IOException;
 import java.util.List;
 
-import net.driftingsouls.ds2.server.ContextCommon;
 import net.driftingsouls.ds2.server.battles.Battle;
 import net.driftingsouls.ds2.server.battles.BattleShip;
-import net.driftingsouls.ds2.server.framework.Common;
 import net.driftingsouls.ds2.server.framework.Context;
 import net.driftingsouls.ds2.server.framework.ContextMap;
 import net.driftingsouls.ds2.server.ships.ShipTypeData;
@@ -39,7 +37,7 @@ public class KSFluchtAllAction extends BasicKSAction {
 	
 	/**
 	 * Prueft, ob das Schiff fliehen kann oder nicht
-	 * @param fluchtmode Der Zeitpunkt der Flucht (<code>current</code> oder <code>next</code>)
+	 * @param fluchtmode Der Zeitpunkt der Flucht (<code>current</code> oder <code>next</code>) - dieser Parameter ist nur noch aus historischen Gruenden vorhanden und hat keinen Effekt mehr
 	 * @param ship Das Schiff
 	 * @param shiptype Der Schiffstyp
 	 * @return <code>true</code>, wenn das Schiff fliehen kann
@@ -59,25 +57,11 @@ public class KSFluchtAllAction extends BasicKSAction {
 		Context context = ContextMap.getContext();
 
 		String fluchtmode = context.getRequest().getParameterString("fluchtmode");
-		
-		if( fluchtmode.equals("current") && (battle.getOwnSide() == 0) && battle.hasFlag(Battle.FLAG_DROP_SECONDROW_1) ) {
-			battle.logme( "Die Flucht nach einem Sturmangriff ist nicht m&ouml;glich\n" );
-			return RESULT_ERROR;
-		}
-		
-		if( fluchtmode.equals("current") && (battle.getOwnSide() == 1) && battle.hasFlag(Battle.FLAG_DROP_SECONDROW_0) ) {
-			battle.logme( "Die Flucht nach einem Sturmangriff ist nicht m&ouml;glich\n" );
-			return RESULT_ERROR;
-		}
 
 		int shipcount = 0;
-		StringBuilder efluchtlog = new StringBuilder();
 		Boolean gotone = null;
 		
-		int fluchtflag = Battle.BS_FLUCHT;
-		if( fluchtmode.equals("next") ) {
-			fluchtflag = Battle.BS_FLUCHTNEXT;
-		}		
+		int fluchtflag = Battle.BS_FLUCHTNEXT;
 		
 		List<BattleShip> ownShips = battle.getOwnShips();
 		for( int i=0; i < ownShips.size(); i++ ) {
@@ -96,10 +80,6 @@ public class KSFluchtAllAction extends BasicKSAction {
 			}
 			
 			if( (aship.getAction() & Battle.BS_JOIN) != 0 ) {
-				continue;
-			}
-
-			if( fluchtmode.equals("current") && aship.getShip().isBattleAction() ) {
 				continue;
 			}
 			 
@@ -140,17 +120,9 @@ public class KSFluchtAllAction extends BasicKSAction {
 				continue;
 			}
 
-			if( fluchtmode.equals("current") ) {
-				battle.logme( aship.getName()+" fl&uuml;chtet\n" );
-				efluchtlog.append(Battle.log_shiplink(aship.getShip())+" flieht\n");
-			
-				aship.setAction(aship.getAction() | Battle.BS_FLUCHT);
-			}
-			else {
-				battle.logme( aship.getName()+" flieht n&auml;chste Runde\n" );
-			
-				aship.setAction(aship.getAction() | Battle.BS_FLUCHTNEXT);
-			}
+		
+			battle.logme( aship.getName()+" flieht n&auml;chste Runde\n" );
+			aship.setAction(aship.getAction() | Battle.BS_FLUCHTNEXT);
 			
 			int remove = 1;
 			for( int j=0; j < ownShips.size(); j++ ) {
@@ -162,24 +134,10 @@ public class KSFluchtAllAction extends BasicKSAction {
 				}
 			}
 			
-			if( fluchtmode.equals("current") ) {
-				if( remove > 1 ) {
-					battle.logme( (remove-1)+" an "+aship.getName()+" gedockte Schiffe fliehen\n" );
-					efluchtlog.append((remove-1)+" an "+aship.getName()+" gedockte Schiffe fliehen\n");
-				}
-			}
-			else {
-				if( remove > 1 ) {
-					battle.logme( (remove-1)+" an "+aship.getName()+" gedockte Schiffe fliehen n&auml;chste Runde\n" );
-				}
+			if( remove > 1 ) {
+				battle.logme( (remove-1)+" an "+aship.getName()+" gedockte Schiffe fliehen n&auml;chste Runde\n" );
 			}
 			shipcount += remove;
-		}
-		
-		if( fluchtmode.equals("current") && (shipcount > 0) ) {	
-			battle.logenemy("<action side=\""+battle.getOwnSide()+"\" time=\""+Common.time()+"\" tick=\""+context.get(ContextCommon.class).getTick()+"\"><![CDATA[\n");
-			battle.logenemy(efluchtlog.toString());
-			battle.logenemy("]]></action>\n");
 		}
 		
 		if( shipcount > 0 ) {
