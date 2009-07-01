7a0719e4c4f88227d8d1b2bf329dd720a2d359f7
Ralf Hajek - rhajek <Ralf Hajek - rhajek@web.de>
2009-07-01 21:13:01 +0200

    [feature] Akademie-Bauschlangen Configwerte

---
+++ db/tables/config_update_offzbaukosten.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO `config` VALUES ('newoffsiliziumcosts', '25', 'Die Siliziumkosten eines neu gebauten Offiziers', '0');
+INSERT INTO `config` VALUES ('newoffnahrungcosts', '35', 'Die Nahrungskosten eines neu gebauten Offiziers', '0');
+INSERT INTO `config` VALUES ('offsiliziumfactor', '1', 'Der Faktor für Siliziumkosten bei Offiziersweiterbildung', '0');
+INSERT INTO `config` VALUES ('offnahrungfactor', '1.5', 'Der Faktor für Nahrungskosten bei Offiziersweiterbildung', '0');
+INSERT INTO `config` VALUES ('offdauercosts', '8', 'Die Dauer eines neu gebauten Offiziers', '0');
+INSERT INTO `config` VALUES ('offdauerfactor', '0.25', 'Der Faktor für die Dauer bei Offiziersweiterbildung', '0');
