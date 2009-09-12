e20ca0e2c746a5ee557564238feb884e82addc8a
Sebastian Gift <devnull@localhost>
2009-09-12 14:36:37 +0200

    [checkstyle]

--- src/net/driftingsouls/ds2/server/ships/ShipBaubar.java
+++ src/net/driftingsouls/ds2/server/ships/ShipBaubar.java
@@ -26,10 +26,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import org.hibernate.annotations.Immutable;
-import org.hibernate.annotations.Type;
-
 import net.driftingsouls.ds2.server.cargo.Cargo;
+
+import org.hibernate.annotations.Type;
 
 /**
  * Ein Baueintrag fuer einen Schiffstyp.
