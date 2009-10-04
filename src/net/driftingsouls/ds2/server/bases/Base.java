b5b3b1a3b674998617218a9a637be1528fbea53d
Mad <devnull@localhost>
2009-10-04 15:27:26 +0200

    [bug]Keine NPE mehr bei Astis ohne Core.

--- src/net/driftingsouls/ds2/server/bases/Base.java
+++ src/net/driftingsouls/ds2/server/bases/Base.java
@@ -1010,8 +1010,11 @@ public class Base implements Cloneable, Lifecycle, Locatable, Transfering
 		int coreId = getCore();
 		Core core = (Core)db.get(Core.class, coreId);
 		
-		balance += core.getProduces().getResourceCount(Resources.RE);
-		balance -= core.getConsumes().getResourceCount(Resources.RE);
+		if(core != null)
+		{
+			balance += core.getProduces().getResourceCount(Resources.RE);
+			balance -= core.getConsumes().getResourceCount(Resources.RE);
+		}
 		
 		return balance;
 	}
