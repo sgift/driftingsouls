5ccdfc70dabc6b5ee06398b15dbe95d78f6611a8
Mad <devnull@localhost>
2010-05-22 23:48:40 +0200

    [bug]Hibernate kann manchmal nicht erkennen, dass ein Schiff keine Module haben muss. In dem Fall den Fehler ignorieren und trotzdem weitermachen.

--- src/net/driftingsouls/ds2/server/ships/Ship.java
+++ src/net/driftingsouls/ds2/server/ships/Ship.java
@@ -92,6 +92,8 @@ import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Cache;
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 import org.hibernate.annotations.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
@@ -120,6 +122,7 @@ public class Ship implements Locatable,Transfering {
 	@OneToOne
 	@JoinColumn(name="modules", nullable=true)
 	@BatchSize(size=50)
+	@NotFound(action = NotFoundAction.IGNORE)
 	private ShipModules modules;
 	@ManyToOne(fetch=FetchType.LAZY)
 	@JoinColumn(name="owner", nullable=false)
