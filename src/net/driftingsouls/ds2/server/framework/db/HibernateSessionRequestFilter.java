0ca0452279872deb587f1029338f77a558e18190
Bernhard Ludemann <berniludemann@gmx.de>
2010-10-13 22:08:11 +0200

    [checkstyle] Meckert trotzdem noch
     user: Bernhard Ludemann <berniludemann@gmx.de>
     changed src/net/driftingsouls/ds2/server/framework/db/HibernateSessionRequestFilter.java

--- src/net/driftingsouls/ds2/server/framework/db/HibernateSessionRequestFilter.java
+++ src/net/driftingsouls/ds2/server/framework/db/HibernateSessionRequestFilter.java
@@ -11,7 +11,6 @@ import javax.servlet.ServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.hibernate.FlushMode;
 import org.hibernate.SessionFactory;
 import org.hibernate.StaleObjectStateException;
 
