70319df6d7661e1842c31d9ebc5b436bc03d7a8f
Christopher Jung <bktheg@web.de>
2011-02-20 15:53:37 +0100

    [ref] Alle Tabellen des alten Umfrage-Systems entfernt

--- db/tables/survey_entries_create.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `survey_entries` (
-  `id` int(11) NOT NULL auto_increment,
-  `survey_id` smallint(5) unsigned NOT NULL default '0',
-  `name` text NOT NULL,
-  `type` varchar(15) NOT NULL default '',
-  `params` text NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `surveys_id` (`survey_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
--- db/tables/survey_results_alter.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE survey_results ADD CONSTRAINT survey_results_fk_surveys FOREIGN KEY (survey_id) REFERENCES surveys(id);
