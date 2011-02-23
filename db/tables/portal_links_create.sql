e158bb89edf3594615757c4a98bea004cf408f22
Christopher Jung <bktheg@web.de>
2011-02-23 18:32:50 +0100

    [ref] portal_links und portal_downloads entfernt

--- db/tables/portal_links_create.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `portal_links` (
-  `id` smallint(5) unsigned NOT NULL auto_increment,
-  `url` varchar(250) NOT NULL default '',
-  `name` varchar(120) NOT NULL default '',
-  `descrip` varchar(255) NOT NULL default '',
-  PRIMARY KEY  (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Links';
- 
