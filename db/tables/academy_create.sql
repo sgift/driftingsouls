b0919c8c20b195982cd5416ef2780d6dd289fa75
Christopher Jung <bktheg@web.de>
2011-02-25 17:59:32 +0100

    [ref] Tabelle academy bereinigt

--- db/tables/academy_create.sql
+++ db/tables/academy_create.sql
@@ -1,8 +1,6 @@
 CREATE TABLE `academy` (
   `id` int(11) NOT NULL auto_increment,
   `train` tinyint(4) NOT NULL default '0',
-  `remain` tinyint(4) NOT NULL default '0',
-  `upgrade` varchar(10) NOT NULL default '',
   `version` int(10) unsigned not null default '0',
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
