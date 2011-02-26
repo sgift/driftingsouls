33064b2bd799373767cefa19a708e8305bee1d5a
Christopher Jung <bktheg@web.de>
2011-02-26 12:48:16 +0100

    [ref] Datenbank->Artikel sowie den damit verbundenen TechDatabaseController und die Tabelle portal_facts entfernt.

--- db/tables/portal_facts_create.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `portal_facts` (
-  `id` mediumint(8) unsigned NOT NULL auto_increment,
-  `class` varchar(16) NOT NULL default '',
-  `title` varchar(64) NOT NULL default '',
-  `text` text NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `class` (`class`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Kleine Artikel zu diversen Dingen aus FS und DS (Schiffe/Ges';
