e7ba3cfaa8045d3391374ca69ec2595052eb215a
bktheg <devnull@localhost>
2012-05-12 10:18:59 +0200

    [ref] Jetty-Version auf 1.8.3 aktualisiert

---
+++ build-utils/jetty/src/net/driftingsouls/ds2/server/embedded/RunEmbedded.java
@@ -0,0 +1,33 @@
+package net.driftingsouls.ds2.server.embedded;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ * Starter fuer Jetty mit integrierter DS-Version.
+ * @author christopherjung
+ *
+ */
+public class RunEmbedded {
+
+	/**
+	 * Main.
+	 * <p>Es wird genau ein Argument unterstuetzt, der Pfad zur DS-Webapp (Verzeichnis oberhalb
+	 * von WEB-INF).
+	 * @param args Die Argumente
+	 * @throws Exception
+	 */
+	public static void main(String[] args) throws Exception {
+		Server server = new Server(8080);
+
+		WebAppContext webapp = new WebAppContext();
+		webapp.setContextPath("/driftingsouls");
+		webapp.setWar(args[0]);
+		webapp.setParentLoaderPriority(true);
+		server.setHandler(webapp);
+
+		server.start();
+		server.join();
+	}
+
+}
