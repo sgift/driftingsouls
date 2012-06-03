bf4cb4c537621d555534b5c27d4da6f889cce9ca
Christopher Jung (bKtHeG) <devnull@localhost>
2012-06-03 22:19:57 +0200

    [bug] Um Speicherprobleme im Werfttick zu vermeiden koennen pro Werft(komplex) nur noch maximal 60 Schiffe in einem Tick fertig gestellt werden.

--- src/net/driftingsouls/ds2/server/tick/regular/WerftTick.java
+++ src/net/driftingsouls/ds2/server/tick/regular/WerftTick.java
@@ -43,7 +43,7 @@ import org.hibernate.Transaction;
 public class WerftTick extends TickController {
 
 	@Override
-	protected void prepare() {	
+	protected void prepare() {
 		// EMPTY
 	}
 
@@ -55,7 +55,7 @@ public class WerftTick extends TickController {
 		db.setFlushMode(FlushMode.MANUAL);
 		Transaction transaction = db.beginTransaction();
 		final User sourceUser = (User)db.get(User.class, -1);
-		
+
 		List<WerftObject> werften = db.createQuery("from ShipWerft s inner join fetch s.ship")
 			.list();
 		werften.addAll(db.createQuery("from BaseWerft b inner join fetch b.base")
@@ -63,7 +63,7 @@ public class WerftTick extends TickController {
 		werften.addAll(db.createQuery("from WerftKomplex")
 			.list());
 		int count = 0;
-		for( Iterator<?> iter=werften.iterator(); iter.hasNext(); ) 
+		for( Iterator<?> iter=werften.iterator(); iter.hasNext(); )
 		{
 			try
 			{
@@ -74,7 +74,7 @@ public class WerftTick extends TickController {
 				transaction.rollback();
 				transaction = db.beginTransaction();
 			}
-			
+
 			count++;
 			int MAX_UNFLUSHED_OBJECTS = 50;
 			if(count%MAX_UNFLUSHED_OBJECTS == 0)
@@ -90,48 +90,51 @@ public class WerftTick extends TickController {
 	}
 
 	private void processWerft(final User sourceUser, WerftObject werft) {
-		try 
+		try
 		{
 			org.hibernate.Session db = getDB();
-			
-			if( (werft instanceof ShipWerft) && (((ShipWerft)werft).getShipID() < 0) ) 
+
+			if( (werft instanceof ShipWerft) && (((ShipWerft)werft).getShipID() < 0) )
 			{
 				return;
 			}
-			
+
 			User owner = werft.getOwner();
 			if( (werft instanceof WerftKomplex) && !((WerftKomplex)werft).isExistant())
 			{
 				return;
 			}
-			if( (owner.getVacationCount() > 0) && (owner.getWait4VacationCount() == 0) ) 
+			if( (owner.getVacationCount() > 0) && (owner.getWait4VacationCount() == 0) )
 			{
 				this.log("xxx Ignoriere Werft "+werft.getWerftID()+" [VAC]");
 				return;
 			}
 			this.log("+++ Werft "+werft.getWerftID()+":");
-			
-			if( !werft.isBuilding() ) 
+
+			if( !werft.isBuilding() )
 			{
 				return;
 			}
+
+			int maxCompleted = 60;
+
 			WerftQueueEntry[] entries = werft.getScheduledQueueEntries();
-			for( int i=0; i < entries.length; i++ ) 
+			for( int i=0; i < entries.length; i++ )
 			{
 				WerftQueueEntry entry = entries[i];
-				
+
 				ShipTypeData shipd = entry.getBuildShipType();
-				
+
 				this.log("\tAktueller Auftrag: "+shipd.getTypeId()+"; dauer: "+entry.getRemainingTime());
-				
-				if( entry.getRequiredItem() > -1 ) 
+
+				if( entry.getRequiredItem() > -1 )
 				{
 					Item item = (Item)db.get(Item.class, entry.getRequiredItem());
 					this.log("\tItem benoetigt: "+item.getName()+" ("+entry.getRequiredItem()+")");
 				}
-				
+
 				// Wenn keine volle Crew vorhanden ist, besteht hier die Moeglichkeit, dass nicht weitergebaut wird.
-				if( Math.random() <= werft.getWorkerPercentageAvailable() ) 
+				if( Math.random() <= werft.getWorkerPercentageAvailable() )
 				{
 					if( entry.isBuildContPossible() )
 					{
@@ -139,32 +142,42 @@ public class WerftTick extends TickController {
 						this.log("\tVoraussetzungen erfuellt - bau geht weiter");
 					}
 				}
-				else 
+				else
 				{
 					this.log("Bau wegen Arbeitermangel pausiert: "+werft.getWorkerPercentageAvailable());
 				}
-				
+
 				if( entry.getRemainingTime() <= 0 ) {
 					this.log("\tSchiff "+shipd.getTypeId()+" gebaut");
 
 					int shipid = entry.finishBuildProcess();
 					this.slog(entry.MESSAGE.getMessage());
-					
+
 					if( shipid > 0 ) {
 						// MSG
 						String msg = "Auf "+werft.getName()+" wurde eine "+shipd.getNickname()+" gebaut. Sie steht bei "+werft.getLocation().displayCoordinates(false)+".";
-					
+
 						PM.send(sourceUser, werft.getOwner().getId(), "Schiff gebaut", msg);
+					}
+
+					if( --maxCompleted <= 0 )
+					{
+						this.log("Maximum an fertige Schiffen erreicht - abbruch");
+						PM.send(sourceUser, werft.getOwner().getId(), "Geplante Auslieferungen",
+								"Auf "+werft.getName()+" wurde die maximale Anzahl an gleichzeig zu produzierenden Schiffen erreicht. " +
+								"Weitere Fertigstellungen wurden von der Raumsicherheit als auch vom Arbeitsschutzbeauftragten der auf den " +
+								"Werften vertretenen Gewerkschaften abgeleht. Der Weiterbau wird beim nächsten Tick automatisch wieder aufgenommen.\n\ngez.\nKoordinationsbüro Werftkomplexe");
+						break;
 					}
 				}
 			}
 		}
-		catch( RuntimeException e ) 
+		catch( RuntimeException e )
 		{
 			this.log("Werft "+werft.getWerftID()+" failed: "+e);
 			e.printStackTrace();
 			Common.mailThrowable(e, "WerftTick Exception", "werft: "+werft.getWerftID());
-			
+
 			throw e;
 		}
 	}
