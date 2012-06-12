c5c306dc9502cd3da2a8e9b84d1eecbd1ccab60a
Christopher Jung (bKtHeG) <devnull@localhost>
2012-06-12 18:16:49 +0200

    [bug] Admin-Kommandos gefixt

--- src/net/driftingsouls/ds2/server/AdminCommands.java
+++ src/net/driftingsouls/ds2/server/AdminCommands.java
@@ -80,7 +80,7 @@ import org.hibernate.ScrollableResults;
  */
 public class AdminCommands {
 	private Map<String,Class<? extends Command>> cmds = new HashMap<String,Class<? extends Command>>();
-	
+
 	/**
 	 * Konstruktor.
 	 */
@@ -97,25 +97,25 @@ public class AdminCommands {
 		cmds.put("clearcaches", ClearCachesCommand.class);
 		cmds.put("tick", TickCommand.class);
 	}
-	
+
 	/**
 	 * Gibt zu einem Admin-Befehl eine Liste moeglicher Vervollstaendigungen
-	 * zurueck. Die Vorschlaege koennen dabei beschreibender Natur sein (Dokumentation). 
+	 * zurueck. Die Vorschlaege koennen dabei beschreibender Natur sein (Dokumentation).
 	 * @param cmd Der Befehl
 	 * @return Die moeglichen Vervollstaendigungen
 	 */
 	public List<String> autoComplete(String cmd) {
 		List<String> result = new ArrayList<String>();
-		
+
 		String[] command = StringUtils.split(cmd, ' ');
 		if( command.length == 0 ) {
 			return result;
 		}
-		
+
 		if( !cmds.containsKey(command[0]) ) {
 			return new ArrayList<String>(cmds.keySet());
 		}
-		
+
 		List<String> subAutoCompletes;
 		try
 		{
@@ -129,11 +129,11 @@ public class AdminCommands {
 		{
 			throw new IllegalStateException(e);
 		}
-		
+
 		for( String ac : subAutoCompletes ) {
 			result.add(command[0]+" "+ac);
 		}
-		
+
 		return result;
 	}
 
@@ -155,7 +155,7 @@ public class AdminCommands {
 		if( cmds.containsKey(command[0]) ) {
 			try
 			{
-				Command cmdExecuter = cmds.get(cmd).newInstance();
+				Command cmdExecuter = cmds.get(command[0]).newInstance();
 				output = cmdExecuter.execute(context, command);
 			}
 			catch( InstantiationException e )
@@ -182,7 +182,7 @@ public class AdminCommands {
 		public String execute(Context context, String[] command);
 		public List<String> autoComplete(String[] command);
 	}
-	
+
 	protected static class TickCommand implements Command {
 		@Override
 		public String execute(Context context, String[] command)
@@ -252,9 +252,9 @@ public class AdminCommands {
 		@Override
 		public String execute( Context context, String[] command ) {
 			String output = "Caches geleert";
-	
+
 			CacheManager.getInstance().clearCaches();
-	
+
 			return output;
 		}
 
@@ -270,18 +270,18 @@ public class AdminCommands {
 		public String execute( Context context, String[] command ) {
 			String output = "";
 			org.hibernate.Session db = context.getDB();
-	
+
 			if( !NumberUtils.isNumber(command[1]) ) {
 				return "Ungueltige Schiffs-ID";
 			}
-	
+
 			int sid = Integer.parseInt(command[1]);
-	
+
 			Ship ship = (Ship)db.get(Ship.class, sid);
 			if( ship == null ) {
 				return "Schiff '"+sid+"' nicht gefunden";
 			}
-	
+
 			if( command[2].equals("heat") ) {
 				if( !NumberUtils.isNumber(command[3]) ) {
 					return "Ueberhitzung ungueltig";
@@ -311,11 +311,11 @@ public class AdminCommands {
 			}
 			else if( command[2].equals("pos") ) {
 				Location loc = Location.fromString(command[3]);
-	
+
 				ship.setSystem(loc.getSystem());
 				ship.setX(loc.getX());
 				ship.setY(loc.getY());
-	
+
 				db.createQuery("update Ship set system=?,x=?,y=? where id>0 and docked in (?,?)")
 					.setInteger(0, loc.getSystem())
 					.setInteger(1, loc.getX())
@@ -347,7 +347,7 @@ public class AdminCommands {
 			}
 			else if( command[2].equals("info") ) {
 				ShipTypeData shiptype = ship.getTypeData();
-	
+
 				output += "Schiff: "+sid+"\n";
 				output += "Typ: "+shiptype.getNickname()+" ("+ship.getType()+")\n";
 				output += "Besitzer: "+ship.getOwner().getId()+"\n";
@@ -368,55 +368,55 @@ public class AdminCommands {
 					return "Slot ungueltig";
 				}
 				int slot = Integer.parseInt(command[3]);
-	
+
 				if( !NumberUtils.isNumber(command[4]) ) {
 					return "Item-ID ungueltig";
 				}
 				int itemid = Integer.parseInt(command[4]);
 				Item item = (Item)db.get(Item.class, itemid);
-	
+
 				if( (item == null) || (item.getEffect().getType() != ItemEffect.Type.MODULE) ) {
 					return "Das Item passt nicht";
 				}
-	
+
 				ship.addModule( slot, ModuleType.ITEMMODULE, Integer.toString(itemid) );
-	
+
 				ShipTypeData shiptype = ship.getTypeData();
-	
+
 				if( ship.getHull() > shiptype.getHull() ) {
 					ship.setHull(shiptype.getHull());
 				}
-	
+
 				if( ship.getShields() > shiptype.getShields() ) {
 					ship.setShields(shiptype.getShields());
 				}
-	
+
 				if( ship.getEnergy() > shiptype.getEps() ) {
 					ship.setEnergy(shiptype.getEps());
 				}
-	
+
 				if( ship.getCrew() > shiptype.getCrew() ) {
 					ship.setCrew(shiptype.getCrew());
 				}
-	
+
 				if( shiptype.getWerft() != 0 ) {
 					ShipWerft werft = (ShipWerft)db.createQuery("from ShipWerft where shipid=?")
 						.setInteger(0, ship.getId())
 						.uniqueResult();
-	
+
 					if( werft == null ) {
 						werft = new ShipWerft(ship);
 						db.persist(werft);
 					}
 				}
-	
+
 				output = "Modul '"+item.getName()+"'@"+slot+" eingebaut\n";
 			}
 			else {
 				output = "Unknown editship sub-command >"+command[2]+"<";
 			}
 			ship.recalculateShipStatus();
-	
+
 			return output;
 		}
 
@@ -426,11 +426,11 @@ public class AdminCommands {
 			if( command.length == 1 ) {
 				return Arrays.asList("<shipId> ...");
 			}
-			if( command.length == 2 || 
+			if( command.length == 2 ||
 					!Arrays.asList("heat","engine","weapons","jumptarget","e","pos","hull","shields","crew","lock","info","additemmodule").contains(command[2]) ) {
 				return Arrays.asList(command[1]+" <heat|engine|weapons|jumptarget|e|pos|hull|shields|crew|lock|info|additemmodule> ...");
 			}
-			
+
 			if( "pos".equals(command[2]) ) {
 				return Arrays.asList(command[1]+" "+command[2]+" <location>");
 			}
@@ -452,7 +452,7 @@ public class AdminCommands {
 		@Override
 		public String execute(Context context, String[] command) {
 			String output = "";
-	
+
 			String oid = command[1];
 			ResourceID resid = null;
 			try {
@@ -461,18 +461,18 @@ public class AdminCommands {
 			catch( RuntimeException e ) {
 				return "Die angegebene Resource ist ungueltig";
 			}
-	
+
 			if( !NumberUtils.isNumber(command[3]) ) {
 				return "Menge ungueltig";
 			}
 			long count = Long.parseLong(command[3]);
-	
+
 			org.hibernate.Session db = context.getDB();
-	
+
 			if( !NumberUtils.isNumber(oid.substring(1)) ) {
 				return "ID ungueltig";
 			}
-	
+
 			Cargo cargo = null;
 			if( oid.startsWith("b") ) {
 				Base base = (Base)db.get(Base.class, Integer.parseInt(oid.substring(1)));
@@ -488,9 +488,9 @@ public class AdminCommands {
 				}
 				cargo = new Cargo(ship.getCargo());
 			}
-	
+
 			cargo.addResource( resid, count );
-	
+
 			if( oid.startsWith("s") ) {
 				Ship ship = (Ship)db.get(Ship.class, Integer.parseInt(oid.substring(1)));
 				ship.setCargo(cargo);
@@ -500,7 +500,7 @@ public class AdminCommands {
 				Base base = (Base)db.get(Base.class, Integer.parseInt(oid.substring(1)));
 				base.setCargo(cargo);
 			}
-	
+
 			return output;
 		}
 
@@ -516,23 +516,23 @@ public class AdminCommands {
 		public String execute(Context context, String[] command) {
 			String output = "";
 			org.hibernate.Session db = context.getDB();
-	
+
 			String cmd = command[1];
 			if( cmd.equals("end") ) {
 				int rqid = Integer.parseInt(command[2]);
-	
+
 				ScriptEngine scriptparser = context.get(ContextCommon.class).getScriptParser("DSQuestScript");
 				final Bindings engineBindings = scriptparser.getContext().getBindings(ScriptContext.ENGINE_SCOPE);
-	
+
 				scriptparser.getContext().setErrorWriter(new NullLogger());
-	
+
 				RunningQuest runningquest = (RunningQuest)db.get(RunningQuest.class, rqid);
-	
+
 				if( !runningquest.getUninstall().isEmpty() ) {
 					engineBindings.put("USER", runningquest.getUser().getId());
 					engineBindings.put("QUEST", "r"+rqid);
 					engineBindings.put("_PARAMETERS", "0");
-	
+
 					try {
 						scriptparser.eval( runningquest.getUninstall() );
 					}
@@ -540,7 +540,7 @@ public class AdminCommands {
 						throw new RuntimeException(e);
 					}
 				}
-	
+
 				db.delete(runningquest);
 			}
 			else if( cmd.equals("list") ) {
@@ -548,7 +548,7 @@ public class AdminCommands {
 				List<?> rquestList = db.createQuery("from RunningQuest rq inner join fetch rq.quest").list();
 				for( Iterator<?> iter=rquestList.iterator(); iter.hasNext(); ) {
 					RunningQuest rquest = (RunningQuest)iter.next();
-	
+
 					output += "* "+rquest.getId()+" - "+rquest.getQuest().getName()+" ("+rquest.getQuest().getId()+") - userid "+rquest.getUser().getId()+"\n";
 				}
 			}
@@ -569,23 +569,23 @@ public class AdminCommands {
 		@Override
 		public String execute(Context context, String[] command) {
 			String output = "";
-	
+
 			String cmd = command[1];
 			if( cmd.equals("end") ) {
 				int battleid = Integer.parseInt( command[2] );
 				org.hibernate.Session db = context.getDB();
-	
+
 				Battle battle = (Battle)db.get(Battle.class, battleid);
-	
+
 				if( battle == null ) {
 					return "Die angegebene Schlacht existiert nicht\n";
 				}
-	
+
 				User sourceUser = (User)context.getDB().get(User.class, -1);
-	
+
 				PM.send(sourceUser, battle.getCommander(0).getId(), "Schlacht beendet", "Die Schlacht bei "+battle.getLocation().displayCoordinates(false)+" wurde durch die Administratoren beendet");
 				PM.send(sourceUser, battle.getCommander(1).getId(), "Schlacht beendet", "Die Schlacht bei "+battle.getLocation().displayCoordinates(false)+" wurde durch die Administratoren beendet");
-	
+
 				battle.load(battle.getCommander(0), null, null, 0);
 				battle.endBattle(0, 0, false);
 			}
@@ -606,7 +606,7 @@ public class AdminCommands {
 		@Override
 		public String execute(Context context, String[] command) {
 			String output = "";
-	
+
 			List<String> sql = new ArrayList<String>();
 			for( int i=1; i < command.length; i++ ) {
 				if( command[i].equals("sector") ) {
@@ -632,20 +632,20 @@ public class AdminCommands {
 			}
 			if( sql.size() > 0 ) {
 				org.hibernate.Session db = context.getDB();
-	
+
 				List<?> ships = db.createQuery("from Ship where "+Common.implode(" and ",sql)).list();
 				int num = ships.size();
 				for( Iterator<?> iter=ships.iterator(); iter.hasNext(); ) {
 					Ship aship = (Ship)iter.next();
 					aship.destroy();
 				}
-	
+
 				output = num+" Schiffe entfernt";
 			}
 			else {
 				output = "Bitte Eingabe konkretisieren (Keine Einschraenkungen vorhanden)";
 			}
-	
+
 			return output;
 		}
 
@@ -658,12 +658,12 @@ public class AdminCommands {
 
 	protected static class BuildImgsCommand implements Command {
 		private static final Log log = LogFactory.getLog(BuildImgsCommand.class);
-		
+
 		private void checkImage( String baseimg, String fleet ) {
 			if( new File(baseimg+fleet+"+.png").isFile() ) {
 				return;
 			}
-	
+
 			try {
 				Font font = null;
 				if( !new File(Configuration.getSetting("ABSOLUTE_PATH")+"data/bnkgothm.ttf").isFile() ) {
@@ -677,19 +677,19 @@ public class AdminCommands {
 					font = Font.createFont(Font.TRUETYPE_FONT,
 							new File(Configuration.getSetting("ABSOLUTE_PATH")+"data/bnkgothm.ttf"));
 				}
-	
+
 				BufferedImage baseImage = ImageIO.read(new FileInputStream(baseimg+".png"));
 				BufferedImage image = new BufferedImage(baseImage.getWidth(), baseImage.getHeight(), BufferedImage.TYPE_INT_RGB);
-	
+
 				Color red = new Color(255,95,95);
 				Color green = new Color(55,255,55);
 				Color blue = new Color(127,146,255);
-	
+
 				Graphics2D g = image.createGraphics();
 				g.drawImage(baseImage, 0, 0, image.getWidth(), image.getHeight(), 0, 0, image.getWidth(), image.getHeight(), null);
-	
+
 				g.setFont(font.deriveFont(12f));
-	
+
 				String[] fleets = StringUtils.splitPreserveAllTokens(fleet, '_');
 				if( fleets.length >= 4 ) {
 					g.setColor(green);
@@ -706,33 +706,33 @@ public class AdminCommands {
 					}
 					g.setColor(textcolor);
 					g.drawString("F", 4, 15);
-	
+
 					textcolor = blue;
 					if( Common.inArray("fe",fleets) ) {
 						textcolor = red;
 					}
-	
+
 					g.setColor(textcolor);
 					g.drawString("F", 12, 15);
 				}
 				else if( fleets.length == 2 ) {
 					Color textcolor = red;
-	
+
 					if( fleets[1].equals("fo") )  {
 						textcolor = green;
 					}
 					else if( fleets[1].equals("fa") ) {
 						textcolor = blue;
 					}
-	
+
 					g.setColor(textcolor);
 					g.drawString("F", 8, 15);
 				}
-	
+
 				g.dispose();
-	
+
 				ImageIO.write(image, "png", new File(baseimg+fleet+".png"));
-	
+
 			}
 			catch( FontFormatException e ) {
 				log.error(e, e);
@@ -741,34 +741,34 @@ public class AdminCommands {
 				log.error(e, e);
 			}
 		}
-	
+
 		private String splitplanetimgs( String baseimg, String targetname ) {
 			String datadir = Configuration.getSetting("ABSOLUTE_PATH")+"data/starmap/";
-	
+
 			baseimg = datadir+baseimg;
 			targetname = datadir+targetname;
-	
+
 			if( !new File(baseimg+".png").isFile() ) {
 				return "FATAL ERROR: bild existiert nicht ("+baseimg+".png)<br />\n";
 			}
-	
+
 			try {
 				BufferedImage image = ImageIO.read(
 						new BufferedInputStream(
 								AdminCommands.class.getClassLoader().getResourceAsStream(baseimg+".png")
 						)
 				);
-	
+
 				int width = image.getWidth() / 25;
 				int height = image.getHeight() / 25;
 				if( width != height ) {
 					return "FATAL ERROR: ungueltige Bildgroesse<br />\n";
 				}
-	
+
 				int size = (width - 1) / 2;
 				int cx = size + 1;
 				int cy = size + 1;
-	
+
 				int index = 0;
 				for( int y=0; y < height; y++ ) {
 					for( int x=0; x < width; x++ ) {
@@ -778,11 +778,11 @@ public class AdminCommands {
 						BufferedImage img = new BufferedImage(25,25, image.getType());
 						Graphics2D g = img.createGraphics();
 						g.drawImage(image, 0, 0, 25, 25, x*25, y*25, x*25+25, y*25+25, null);
-	
+
 						g.dispose();
-	
+
 						ImageIO.write(img, "png", new File(targetname+index+".png"));
-	
+
 						index++;
 					}
 				}
@@ -790,21 +790,21 @@ public class AdminCommands {
 			catch( IOException e ) {
 				return e.toString();
 			}
-	
+
 			return "";
 		}
-	
+
 		@Override
 		public String execute(Context context, String[] command) {
 			String output = "";
-	
+
 			String cmd = command[1];
 			if( cmd.equals("starmap") ) {
 				String img = command[2];
-	
+
 				boolean sizedimg = false;
 				int imgcount = 0;
-	
+
 				String path = Configuration.getSetting("ABSOLUTE_PATH")+"data/starmap/"+img+"/"+img;
 				if( !new File(path+"0.png").isFile() ) {
 					if( !new File(path+".png").isFile() ) {
@@ -814,11 +814,11 @@ public class AdminCommands {
 				else {
 					sizedimg = true;
 				}
-	
+
 				while( true ) {
 					for( int i=0; i < 8; i++ ) {
 						String fleet = "";
-	
+
 						if( (i & 4) != 0 ) {
 							fleet += "_fo";
 						}
@@ -828,15 +828,15 @@ public class AdminCommands {
 						if( (i & 1) != 0 ) {
 							fleet += "_fe";
 						}
-	
+
 						if( fleet.length() == 0 ) {
 							continue;
 						}
-	
+
 						if( new File(path+(sizedimg ? imgcount : "")+fleet+".png").isFile() ) {
 							new File(path+(sizedimg ? imgcount : "")+fleet+".png").delete();
 						}
-	
+
 						checkImage(path+(sizedimg ? imgcount : ""),fleet);
 					}
 					if( sizedimg ) {
@@ -853,13 +853,13 @@ public class AdminCommands {
 			else if( cmd.equals("splitplanetimg") ) {
 				String img = command[2];
 				String target = command[3];
-	
+
 				output = splitplanetimgs( img, target );
 			}
 			else {
 				output = "Unbekannter Befehl "+cmd;
 			}
-	
+
 			return output;
 		}
 
@@ -874,10 +874,10 @@ public class AdminCommands {
 		@Override
 		public String execute(Context context, String[] command) {
 			String output = "";
-	
+
 			String taskid = command[1];
 			String message = command[2];
-	
+
 			if( Taskmanager.getInstance().getTaskByID(taskid) != null ) {
 				Taskmanager.getInstance().handleTask(taskid, message);
 				output = "Task ausgef&uuml;hrt";
@@ -885,7 +885,7 @@ public class AdminCommands {
 			else {
 				output = "Keine g&uuml;ltige TaskID";
 			}
-	
+
 			return output;
 		}
 
@@ -901,12 +901,12 @@ public class AdminCommands {
 		public String execute(Context context, String[] command) {
 			String output = "";
 			org.hibernate.Session db = context.getDB();
-	
+
 			ScrollableResults ships = db.createQuery("from Ship as s left join fetch s.modules " +
 													 "where s.id>0 order by s.owner,s.docked,s.shiptype asc")
 													 .setCacheMode(CacheMode.IGNORE)
 													 .scroll(ScrollMode.FORWARD_ONLY);
-	
+
 			int count = 0;
 			long start = System.currentTimeMillis();
 			while(ships.next())
@@ -914,7 +914,7 @@ public class AdminCommands {
 				Ship ship = (Ship) ships.get(0);
 				ship.recalculateModules();
 				count++;
-	
+
 				if(count % 20 == 0)
 				{
 					db.flush();
@@ -923,7 +923,7 @@ public class AdminCommands {
 					HibernateUtil.getSessionFactory().getCurrentSession().evict(Offizier.class);
 				}
 			}
-	
+
 			output = "Es wurden "+count+" Schiffe in "+ (System.currentTimeMillis() - start)/1000d +" Sekunden neu berechnet.";
 			return output;
 		}
