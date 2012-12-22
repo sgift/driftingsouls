f7d979628dc3845e252c8c26d3ea8d957b2e706b
Christopher Jung <bktheg@web.de>
2012-12-22 10:54:55 +0100

    [ref/bug] Versuch eines Workarounds um die Fehler beim persitieren von PermanentSessions

--- src/net/driftingsouls/ds2/server/framework/authentication/PermanentSession.java
+++ src/net/driftingsouls/ds2/server/framework/authentication/PermanentSession.java
@@ -1,6 +1,8 @@
 package net.driftingsouls.ds2.server.framework.authentication;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -13,8 +15,8 @@ import javax.persistence.Table;
 @Table(name="sessions")
 public class PermanentSession
 {
-	@Id
-	private int id;
+	@Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Integer id;
 	private int userId;
 	private String token;
 	private long tick;
@@ -24,7 +26,7 @@ public class PermanentSession
 	/**
 	 * @return Die Id.
 	 */
-	public int getId() 
+	public Integer getId()
 	{
 		return id;
 	}
@@ -32,7 +34,7 @@ public class PermanentSession
 	/**
 	 * @param id Die Id.
 	 */
-	public void setId(int id) 
+	public void setId(Integer id)
 	{
 		this.id = id;
 	}
