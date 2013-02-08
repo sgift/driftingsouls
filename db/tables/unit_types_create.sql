d3a4ed6dd0acd62ad91d14f8b938f0acc5a54c54
Christopher Jung <bktheg@web.de>
2013-02-08 18:33:47 +0100

    [bug] DB-Definitionen gefixt

--- db/tables/unit_types_create.sql
+++ db/tables/unit_types_create.sql
@@ -1,14 +1,15 @@
 CREATE TABLE `unit_types` (
-  			`id` int(11) NOT NULL auto_increment,
-  			`name` varchar(50) NOT NULL,
-  			`size` tinyint(4) NOT NULL default '1',
-  			`description` text,
-  			`recost` float(11) NOT NULL default '0',
-  			`nahrungcost` float(11) NOT NULL default '1',
-  			`kapervalue` int(11) NOT NULL default '1',
-  			`buildcosts` varchar(120) NOT NULL default '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,',
-  			`dauer` int(11) NOT NULL default '1',
-  			`resid` int(11) NOT NULL default '0',
-  			`picture` varchar(50),
-  			PRIMARY KEY  (`id`)
-		);
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`name` varchar(50) NOT NULL,
+	`size` tinyint(4) NOT NULL DEFAULT '1',
+	`description` text,
+	`recost` float NOT NULL,
+	`nahrungcost` float NOT NULL,
+	`kapervalue` int(11) NOT NULL DEFAULT '1',
+	`buildcosts` varchar(120) NOT NULL DEFAULT '0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,',
+	`dauer` int(11) NOT NULL DEFAULT '1',
+	`resid` int(11) NOT NULL DEFAULT '0',
+	`picture` varchar(50) DEFAULT NULL,
+	`hidden` tinyint(1) DEFAULT '0',
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
