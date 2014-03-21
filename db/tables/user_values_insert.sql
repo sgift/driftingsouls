a36cc6cec6a4a7c9c1defa1e3184dc43293e9a45
Christopher Jung <bktheg@web.de>
2014-03-21 19:00:09 +0100

    [ref] Beispieldatensatz etwas bereinigt

--- db/tables/user_values_insert.sql
+++ db/tables/user_values_insert.sql
@@ -1,15 +1,10 @@
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (607, 0, 'GAMEPLAY/bases/maxtiles', '500');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (20, 0, 'GTU_AUCTION_USER_COST', '10');
-INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (2, 0, 'PIRATEN_Ansehen', '15');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (973, 0, 'PMS/signature', '');
-INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (957, 0, 'TBLORDER/admin/show_cmdline', '0');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (6, 0, 'TBLORDER/basen/order', 'name');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (7, 0, 'TBLORDER/basen/order_mode', '0');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (8, 0, 'TBLORDER/basen/showcargo', '1');
-INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (959, 0, 'TBLORDER/clients/jstarmap/bufferedoutput', '0');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (961, 0, 'TBLORDER/factions/konto_maxtype', '2');
-INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (15, 0, 'TBLORDER/map/height', '600');
-INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (14, 0, 'TBLORDER/map/width', '600');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (31, 0, 'TBLORDER/pms/forward', '0');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (12, 0, 'TBLORDER/schiff/sensororder', 'id');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (13, 0, 'TBLORDER/schiff/tooltips', '1');
@@ -19,4 +14,3 @@ INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (10, 0, 'TBL
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (11, 0, 'TBLORDER/schiffe/showjaeger', '0');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (3, 0, 'TBLORDER/uebersicht/box', 'bookmarks');
 INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (4, 0, 'TBLORDER/uebersicht/inttutorial', '5');
-INSERT INTO `user_values` (`id`, `user_id`, `name`, `value`) VALUES (974, 0, 'PMS/signature', '');
