94d07c2aa460e2a2c9fb7f06380377c2adfe3550
bKtHeG <bktheg@web.de>
2014-06-20 14:58:30 +0200

    [ref] Interceptoren sollten nur prüfen. Die Authentifizierung muss daher von der Prüfung, ob ein (berechtigter) Benutzer vorliegt, getrennt werden.

--- src/net/driftingsouls/ds2/server/framework/pipeline/controllers/UserAuthenticationMethodInterceptor.java
+++ src/net/driftingsouls/ds2/server/framework/pipeline/controllers/UserAuthenticationMethodInterceptor.java
@@ -1,9 +1,8 @@
 package net.driftingsouls.ds2.server.framework.pipeline.controllers;
 
+import net.driftingsouls.ds2.server.framework.ContextMap;
 import net.driftingsouls.ds2.server.framework.NotLoggedInException;
-import net.driftingsouls.ds2.server.framework.authentication.AuthenticationManager;
 import org.aopalliance.intercept.MethodInvocation;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 /**
@@ -12,20 +11,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class UserAuthenticationMethodInterceptor implements ActionMethodInterceptor
 {
-	private AuthenticationManager manager;
-
-	@Autowired
-	public UserAuthenticationMethodInterceptor(AuthenticationManager manager)
-	{
-		this.manager = manager;
-	}
-
 	@Override
 	public Object invoke(MethodInvocation methodInvocation) throws Throwable
 	{
-		boolean authenticatedUser = manager.authenticateCurrentSession();
-
-		if(authenticatedUser)
+		if(ContextMap.getContext().getActiveUser() != null)
 		{
 			return methodInvocation.proceed();
 		}
