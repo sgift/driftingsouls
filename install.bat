6e30dfcd520b903283af0b241b349b4fac47f6b6
Sebastian Gift <sebastian.gift@mobilexag.de>
2020-03-07 21:34:29 +0100

    Remove unused files.

--- install.bat
+++
@@ -1,1 +0,0 @@
-java -cp "game/target/driftingsouls-game-0.0.1-SNAPSHOT/WEB-INF/classes:game/target/driftingsouls-game-0.0.1-SNAPSHOT/WEB-INF/lib/*" net.driftingsouls.ds2.server.install.Install %*
--- install.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp "game/target/driftingsouls-game-0.0.1-SNAPSHOT/WEB-INF/classes:game/target/driftingsouls-game-0.0.1-SNAPSHOT/WEB-INF/lib/*" net.driftingsouls.ds2.server.install.Install $*
