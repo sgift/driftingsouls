2ce85eeaa27491d02a5a13b9edad675c60f677e7
Sebastian Gift <sebastian.gift@mobilexag.de>
2020-03-08 22:55:15 +0100

    Add always fill code again without superfluous variable.

--- game/src/main/java/net/driftingsouls/ds2/server/modules/TradepostController.java
+++ game/src/main/java/net/driftingsouls/ds2/server/modules/TradepostController.java
@@ -422,12 +422,8 @@ public class TradepostController extends Controller
 			if (fill)
 			{
 				Cargo cargo = ship.getCargo();
-				long cnt = cargo.getResourceCount(rid);
-				if (cnt < saleslimit)
-				{
-					cargo.setResource(rid, saleslimit);
-					ship.setCargo(cargo);
-				}
+				cargo.setResource(rid, saleslimit);
+				ship.setCargo(cargo);
 			}
 		}
 
