577fc53dc167cbc9cb13811d686c04b21a58c7bb
Syndikat <gregor.fuhs@inform-software.com>
2020-03-25 16:24:38 +0100

    feat: Schiffe an der Werft aufladen

---
+++ game/src/main/java/net/driftingsouls/ds2/server/werften/ReloadCosts.java
@@ -0,0 +1,19 @@
+package net.driftingsouls.ds2.server.werften;
+
+
+
+/**
+ * Die Aufladungskosten eines Schiffes.
+ *
+ */
+public class ReloadCosts
+{
+	/**
+	 * Die Energiekosten.
+	 */
+	public int e;
+
+	ReloadCosts() {
+		//EMPTY
+	}
+}
