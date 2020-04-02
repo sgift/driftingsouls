8d553f32b0414fcc2f34ff24bf1530a8450cfa58
Warg <59847361+WargDS2@users.noreply.github.com>
2020-04-03 00:14:51 +0200

    Umlaute und Rechtschreibfehler
    @sgift 
    Ich habe mal gesucht, aber nicht gefunden, wieso dieser Fehler auftritt:
    http://ds.rnd-it.de/viewtopic.php?f=12&p=453#p453
    
    Siehst du es auf Anhieb und kannst es ggf. fixen?

--- game/src/main/java/net/driftingsouls/ds2/server/bases/Fabrik.java
+++ game/src/main/java/net/driftingsouls/ds2/server/bases/Fabrik.java
@@ -159,8 +159,8 @@ public class Fabrik extends DefaultBuilding
 		{
 			vars.usedcapacity.put(buildingid, BigDecimal.valueOf(-1));
 
-			log.warn("Basis " + base.getId() + " verfuegt ueber keinen Fabrik-Eintrag, obwohl es eine Fabrik hat");
-			return "Basis " + base.getId() + " verfuegt ueber keinen Fabrik-Eintrag, obwohl es eine Fabrik hat";
+			log.warn("Basis " + base.getId() + " verfügt über keinen Fabrik-Eintrag, obwohl es eine Fabrik hat.");
+			return "Basis " + base.getId() + " verfügt über keinen Fabrik-Eintrag, obwohl es eine Fabrik hat.";
 		}
 
 		Factory.Task[] plist = wf.getProduces();
@@ -181,7 +181,7 @@ public class Fabrik extends DefaultBuilding
 			if ((count > 0) && !thisitemslist.contains(entry))
 			{
 				ok = false;
-				wfreason.append("Es existieren nicht die n&ouml;tigen Baupl&auml;ne f&uuml;r ").append(entry.getName()).append("\n");
+				wfreason.append("Es existieren nicht die nötigen Baupläne für ").append(entry.getName()).append("\n");
 				break;
 			}
 		}
@@ -219,7 +219,7 @@ public class Fabrik extends DefaultBuilding
 		else
 		{
 			String basename = base.getName();
-			wfreason.insert(0, "[b]" + basename + "[/b] - Die Arbeiten in der Fabrik zeitweise eingestellt.\nGrund:\n");
+			wfreason.insert(0, "[b]" + basename + "[/b] - Die Arbeiten in der Fabrik sind zeitweise eingestellt.\nGrund:\n");
 		}
 
 		if (!vars.usedcapacity.containsKey(buildingid) || (vars.usedcapacity.get(buildingid).doubleValue() <= 0))
@@ -497,7 +497,7 @@ public class Fabrik extends DefaultBuilding
 
 		if (wf == null)
 		{
-			echo.append("<div style=\"color:red\">FEHLER: Diese Fabrik besitzt keinen Eintrag<br /></div>\n");
+			echo.append("<div style=\"color:red\">FEHLER: Diese Fabrik besitzt keinen Eintrag.<br /></div>\n");
 			return echo.toString();
 		}
 		/*
@@ -555,7 +555,7 @@ public class Fabrik extends DefaultBuilding
 
 			if (entry == null)
 			{
-				echo.append("<span style=\"color:red\">Fehler: Der angegebene Bauplan existiert nicht</span>\n");
+				echo.append("<span style=\"color:red\">Fehler: Der angegebene Bauplan existiert nicht.</span>\n");
 				return echo.toString();
 			}
 
@@ -625,12 +625,12 @@ public class Fabrik extends DefaultBuilding
 
 					wf.setProduces(producelist.toArray(new Factory.Task[0]));
 
-					echo.append(Math.abs(count)).append(" ").append(entry.getName()).append(" wurden ").append((count >= 0 ? "hinzugef&uuml;gt" : "abgezogen")).append("<br /><br />");
+					echo.append(Math.abs(count)).append(" ").append(entry.getName()).append(" wurden ").append((count >= 0 ? "hinzugefügt" : "abgezogen")).append("<br /><br />");
 				}
 			}
 			else
 			{
-				echo.append("Sie haben nicht alle ben&ouml;tigten Forschungen f&uuml;r ").append(entry.getName()).append("<br /><br />");
+				echo.append("Sie haben nicht alle benötigten Forschungen für ").append(entry.getName()).append("<br /><br />");
 			}
 		}
 
@@ -657,7 +657,7 @@ public class Fabrik extends DefaultBuilding
 
 				if (!itemslist.contains(entry))
 				{
-					echo.append("WARNUNG: Ungueltiges Item >").append(entry.getId()).append("< (count: ").append(ammoCount).append(") in der Produktionsliste entdeckt<br />\n");
+					echo.append("WARNUNG: Ungültiges Item >").append(entry.getId()).append("< (count: ").append(ammoCount).append(") in der Produktionsliste entdeckt.<br />\n");
 					continue;
 				}
 
