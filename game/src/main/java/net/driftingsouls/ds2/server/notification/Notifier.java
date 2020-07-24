f1097daf217df3940e2a13ec82cb1b087e2317da
Gregor Fuhs <gregor.fuhs@inform-software.com>
2020-07-24 08:21:21 +0200

    fix: Validierung der Push-Texte

--- game/src/main/java/net/driftingsouls/ds2/server/notification/Notifier.java
+++ game/src/main/java/net/driftingsouls/ds2/server/notification/Notifier.java
@@ -8,36 +8,52 @@ import java.net.URL;
 public class Notifier {
 
 	private final String apiKey;
-	 
+
 	public Notifier(final String apiKey) {
 		this.apiKey = apiKey;
-	}  
-	 
-	public int sendMessage(final String title, final String text){  
-		try {     
-			final URL url =    new URL("https://www.notifymydevice.com/push");  
-            final HttpsURLConnection conn= (HttpsURLConnection)url.openConnection(); 
-            conn.setRequestMethod("POST");             
-            conn.setDoOutput(true);  
-	 
-	        final OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());   
-	        out.write("ApiKey=");        
-	        out.write(apiKey);          
-	        out.write("&PushTitle=");      
-	        out.write(title);         
-	        out.write("&PushText=");        
-	        out.write(text);         
-	        out.flush();        
-	        out.close();  
-	 
-	        
-	        return conn.getResponseCode();    
-	    } 
-		catch (final IOException anException){   
-			anException.printStackTrace();        
-		}      
-		return -1;     
 	}
-	
-	
+
+	public int sendMessage(final String title, final String text){
+		try {
+			final URL url =    new URL("https://www.notifymydevice.com/push");
+            final HttpsURLConnection conn= (HttpsURLConnection)url.openConnection();
+            conn.setRequestMethod("POST");
+						conn.setDoOutput(true);
+
+					String inhalt = parseToValidText(text);
+					String titel = parseToValidText(title);
+
+	        final OutputStreamWriter out = new OutputStreamWriter(conn.getOutputStream());
+	        out.write("ApiKey=");
+	        out.write(apiKey);
+	        out.write("&PushTitle=");
+	        out.write(titel);
+	        out.write("&PushText=");
+	        out.write(inhalt);
+	        out.flush();
+	        out.close();
+
+
+	        return conn.getResponseCode();
+	    }
+		catch (final IOException anException){
+			anException.printStackTrace();
+		}
+		return -1;
+	}
+
+	private String parseToValidText(String input){
+		String output = input;
+
+		//html-Tags entfernen
+		output.replaceAll("\\<. *?>","");
+		//BB Code entfernen
+		output.replaceAll("\[\/?(?i)(b|i|u|color|url|img|email|size|list|font|youtube|ship|map|base|resource|userprofile|align|mark|\*){1}=?[^\]]*\]","");
+		//&-Symbol entfernen, das ist in URLs leider nicht erlaubt
+		output.replace("&"," und ");
+
+		return output;
+	}
+
+
 }
