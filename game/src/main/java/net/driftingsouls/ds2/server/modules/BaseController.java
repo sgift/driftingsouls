59a6b537f8b20c9f6dc3c7076ebbedea6d6ffab9
Gregor <38551264+SC-Syndikat@users.noreply.github.com>
2021-11-13 19:50:51 +0100

    Fix: defaultAction noch anpassen

--- game/src/main/java/net/driftingsouls/ds2/server/modules/BaseController.java
+++ game/src/main/java/net/driftingsouls/ds2/server/modules/BaseController.java
@@ -389,12 +389,21 @@ public class BaseController extends Controller
 		return response;
 	}
 
+        /**
+	 * Zeigt die Basis an.
+	 */
+	@Action(ActionType.DEFAULT)
+	public TemplateEngine defaultAction(@UrlParam(name = "col") Base base, Ship ship, RedirectViewResult redirect) {
+		return defaultAction(base,  null, redirect);
+        }
+
+
 	/**
 	 * Zeigt die Basis an.
 	 */
 	@Action(ActionType.DEFAULT)
 	public TemplateEngine defaultAction(@UrlParam(name = "col") Base base, Ship ship, RedirectViewResult redirect) {
-		boolean scan = ship == null;
+		boolean scan = ship! = null;
 		if (!scan)
 		{
 			validate(base);
