10e1685627e2bb5cad522a82d46df030e880cec8
SC Syndikat <gregor.fuhs@rwth-aachen.de>
2021-12-07 10:53:16 +0100

    fix: die spalte heisst produces

--- game/src/db/migration/20211207.1_module_produces.sql
+++ game/src/db/migration/20211207.1_module_produces.sql
@@ -1,3 +1,3 @@
-alter table ship_types add column cargo longtext NOT NULL;
-alter table ships_modules add column cargo longtext NOT NULL;
-alter table schiffstyp_modifikation add column cargo longtext NOT NULL;
+alter table ship_types add column produces longtext NOT NULL;
+alter table ships_modules add column produces longtext NOT NULL;
+alter table schiffstyp_modifikation add column produces longtext NOT NULL;
