d30bd27ea2b39ff656afd1d19ea6e0d6d746bf0e
SC Syndikat <gregor.fuhs@rwth-aachen.de>
2021-12-07 13:22:32 +0100

    fix: Initialwerte fuer Produktionsspalte

--- game/src/db/migration/20211207.1_module_produces.sql
+++ game/src/db/migration/20211207.1_module_produces.sql
@@ -1,3 +1,7 @@
 alter table ship_types add column produces longtext NOT NULL;
 alter table ships_modules add column produces longtext NOT NULL;
 alter table schiffstyp_modifikation add column produces longtext NOT NULL;
+
+update ship_types set produces = '';
+update ships_modules set produces = '';
+update schiffstyp_modifikation set produces = '';
